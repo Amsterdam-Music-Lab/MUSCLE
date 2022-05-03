0cf1930e60f8678ebff018e3b52233837a2c02bb
albertas-jn <24507839+albertas-jn@users.noreply.github.com>
2022-05-03 15:21:31 +0200

    Add a test for compileplaylist

---
+++ backend/experiment/tests/test_management.py
@@ -0,0 +1,33 @@
+from django.core.management import call_command
+from django.test import TestCase
+import csv
+from django.conf import settings
+from os.path import join
+from os import remove
+ 
+
+class CompilePlaylistTest(TestCase):
+
+    def test_output_csv(self):
+        call_command('compileplaylist', 'tests/compileplaylist')
+        # Load generated csv
+        filename = join(settings.MEDIA_ROOT,'tests','compileplaylist','audiofiles.csv')
+        try:
+            with open(filename) as csv_file:
+                rows = csv.DictReader(csv_file, fieldnames = ('artist','name','start_time','duration','filename','restrict_to_nl','tag_id','group_id'))
+                for row in rows:
+                    if row['filename'] == 'tests/compileplaylist/silence_20sec.wav':
+                        self.assertEqual(row['artist'], 'default')
+                        self.assertEqual(row['name'], 'silence_20sec')
+                        self.assertEqual(row['start_time'], '0.0')
+                        self.assertEqual(row['duration'], '20.025850340136053')
+                        self.assertEqual(row['restrict_to_nl'], '0')
+                        self.assertEqual(row['tag_id'], '0')
+                        self.assertEqual(row['group_id'], '0')
+        finally:        
+            remove(filename)  # Make sure csv file is deleted even if tests fail
+
+
+
+
+
