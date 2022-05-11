50d5fec1f06767a7aad8dc854fb2eb327ae13cf7
Berit <berit.janssen@gmail.com>
2022-05-11 09:40:56 +0200

    Revert "add demographics questions to Gold-MSI"
    This reverts commit 77ca791c84ae30e4db7b10798b05153cf6652af3.

--- backend/experiment/rules/gold_msi.py
+++ backend/experiment/rules/gold_msi.py
@@ -1,10 +1,9 @@
 import logging
 
 from django.utils.translation import gettext_lazy as _
-from .views import Trial, Consent, Explainer, Final, Playlist, StartSession
+from .views import CompositeView, Consent, Explainer, Final, Playlist, StartSession
 from .views.form import Form
 from .util.goldsmiths import MSI_F3_MUSICAL_TRAINING
-from .util.questions import DEMOGRAPHICS, EXTRA_DEMOGRAPHICS, question_by_key
 from .util.actions import combine_actions, final_action_with_optional_button
 
 from .base import Base
@@ -12,15 +11,7 @@ from .base import Base
 class GoldMSI(Base):
     """ an experiment view that implements the GoldMSI questionnaire """
     ID = 'GOLD_MSI'
-    demographics = [
-        question_by_key('dgf_gender_identity'),
-        question_by_key('dgf_age', EXTRA_DEMOGRAPHICS),
-        question_by_key('dgf_education'),
-        question_by_key('dgf_highest_qualification_expectation', EXTRA_DEMOGRAPHICS),
-        question_by_key('dgf_country_of_residence'),
-        question_by_key('dgf_country_of_origin'),
-    ]
-    questions = MSI_F3_MUSICAL_TRAINING + demographics
+    questions = MSI_F3_MUSICAL_TRAINING
 
     @classmethod
     def first_round(cls, experiment):
@@ -40,5 +31,5 @@ class GoldMSI(Base):
         feedback_form = Form([
             question,
         ], is_profile=True)
-        view = Trial(None, feedback_form)
+        view = CompositeView(None, feedback_form.action())
         return view.action()
