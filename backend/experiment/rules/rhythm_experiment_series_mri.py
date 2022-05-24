3f750cd73d93a2e49301ec106aa58fd17723a89e
Berit <berit.janssen@gmail.com>
2022-05-24 14:13:08 +0200

    add consent and debrief form for MRI participants, modify .po file

--- backend/experiment/rules/rhythm_experiment_series_mri.py
+++ backend/experiment/rules/rhythm_experiment_series_mri.py
@@ -2,5 +2,5 @@ from . import RhythmExperimentSeries
 
 class RhythmExperimentSeriesMRI(RhythmExperimentSeries):
     ID = 'RHYTHM_SERIES_MRI'
-    consent_form = 'consent/consent_rhythm_mri.html'
-    debrief_form = 'final/experiment_series_mri.html'
+    consent_form = 'consent/consent_MRI.html'
+    debrief_form = 'final/debrief_MRI.html'
