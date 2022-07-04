55db2b6da39ff25805b96b7885be5812c753d439
Werner <werner@sudox.nl>
2022-07-04 09:17:45 +0200

    style: formatting

--- frontend/src/components/FeedbackForm/FeedbackForm.js
+++ frontend/src/components/FeedbackForm/FeedbackForm.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import Question from "../Question/Question";
 import Button from "../Button/Button";
 
-
 // FeedbackForm
-const FeedbackForm = ({ formActive, form, buttonLabel, skipLabel, isSkippable, onResult, emphasizeTitle = true }) => {
-
-    const showSubmitButtons = form.filter( formElement => formElement.submits).length === 0;
+const FeedbackForm = ({
+    formActive,
+    form,
+    buttonLabel,
+    skipLabel,
+    isSkippable,
+    onResult,
+    emphasizeTitle = true,
+}) => {
+    const showSubmitButtons =
+        form.filter((formElement) => formElement.submits).length === 0;
 
     const [formValid, setFormValid] = useState(false);
-    
+
     const onSubmit = () => {
         // Callback onResult with question data
         onResult({
-            form
+            form,
         });
     };
 
@@ -22,11 +29,14 @@ const FeedbackForm = ({ formActive, form, buttonLabel, skipLabel, isSkippable, o
         if (form[question_key].submits) {
             onSubmit(form);
         }
-        if (form.filter( formElement => formElement.value ).length === form.length) {
+        if (
+            form.filter((formElement) => formElement.value).length ===
+            form.length
+        ) {
             setFormValid(true);
         }
     };
-    
+
     return (
         <div className="aha__feedback d-flex justify-content-center">
             <form>
@@ -39,35 +49,40 @@ const FeedbackForm = ({ formActive, form, buttonLabel, skipLabel, isSkippable, o
                         onChange={onChange}
                         emphasizeTitle={emphasizeTitle}
                     />
-                )
-                )}
+                ))}
                 {/* Continue button */}
                 {showSubmitButtons && formValid && (
-                     <div className="center">
-                <Button
-                    onClick={() => {
-                        onSubmit();
-                    }}
-                    className={"btn-primary anim anim-fade-in anim-speed-500"}
-                    title={buttonLabel}
-                /></div>)}
+                    <div className="center">
+                        <Button
+                            onClick={() => {
+                                onSubmit();
+                            }}
+                            className={
+                                "btn-primary anim anim-fade-in anim-speed-500"
+                            }
+                            title={buttonLabel}
+                        />
+                    </div>
+                )}
 
                 {/* Skip button */}
                 {/* Only show skip-button when there is no value */}
                 {isSkippable && showSubmitButtons && (
                     <div className="center">
                         <Button
-                        onClick={() => {
-                            onSubmit();
-                        }}
-                        className={"btn-gray anim anim-fade-in anim-speed-500"}
-                        title={skipLabel}
-                    />
+                            onClick={() => {
+                                onSubmit();
+                            }}
+                            className={
+                                "btn-gray anim anim-fade-in anim-speed-500"
+                            }
+                            title={skipLabel}
+                        />
                     </div>
                 )}
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default FeedbackForm;
