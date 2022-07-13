375d2165e44bdae8869d5c5514d225e7c08e2c54
Werner <werner@sudox.nl>
2022-07-13 15:12:15 +0200

    fix: restore broken counter functionality

--- frontend/src/components/Score/Score.js
+++ frontend/src/components/Score/Score.js
@@ -4,34 +4,54 @@ import Circle from "../Circle/Circle";
 import Button from "../Button/Button";
 
 // Score is an experiment view that shows an intermediate and total score
-const Score = ({ last_song, score, score_message, total_score, texts, icon, timer, onNext }) => {
+const Score = ({
+    last_song,
+    score,
+    score_message,
+    total_score,
+    texts,
+    icon,
+    timer,
+    onNext,
+}) => {
     const [showScore, setShowScore] = useState(0);
     // Use a ref to prevent doing multiple increments
     // when the render is skipped
     const scoreValue = useRef(0);
 
     useEffect(() => {
-        if (score !== scoreValue.current) {
-            // lower
-            if (scoreValue.current < score) {
-                scoreValue.current += 1;
-                setShowScore(scoreValue.current);
-            } else {
-                scoreValue.current -= 1;
-                setShowScore(scoreValue.current);
+        const id = setTimeout(() => {
+            if (score !== scoreValue.current) {
+                // lower
+                if (scoreValue.current < score) {
+                    scoreValue.current += 1;
+                    setShowScore(scoreValue.current);
+                } else {
+                    scoreValue.current -= 1;
+                    setShowScore(scoreValue.current);
+                }
             }
-        }
+        }, 50);
+
+        return () => {
+            window.clearTimeout(id);
+        };
+    }, [score,showScore]);
+
+    useEffect(() => {
+        let id = -1;
         if (timer) {
-            const id = setTimeout(() => {
-                onNext(); 
+            id = setTimeout(() => {
+                onNext();
             }, timer * 1000);
-            return () => {
-                clearTimeout(id);
-            };
         }
-        
-        
-    }, [score, showScore, timer, onNext]);
+
+        return () => {
+            if (timer) {
+                clearTimeout(id);
+            }
+        };
+    }, [timer, onNext]);
 
     return (
         <div className="aha__score d-flex flex-column justify-content-center">
@@ -44,31 +64,35 @@ const Score = ({ last_song, score, score_message, total_score, texts, icon, time
             >
                 <Circle />
                 <div className="content">
-                {!icon ? 
-                <div>
-                    <h1>
-                        {score > 0 && "+"}
-                        {showScore}
-                    </h1>
-                    <h3>{score_message}</h3>
-                    </div>
-                    : <span className={icon}></span>
-                }
+                    {!icon ? (
+                        <div>
+                            <h1>
+                                {score > 0 && "+"}
+                                {showScore}
+                            </h1>
+                            <h3>{score_message}</h3>
+                        </div>
+                    ) : (
+                        <span className={icon}></span>
+                    )}
                 </div>
             </div>
-            <h4 className="total-score">
-                {texts.score}: {total_score - score + showScore}
-            </h4>
+            {total_score === 0 ||
+                (!!total_score && (
+                    <h4 className="total-score">
+                        {texts.score}: {total_score - score + showScore}
+                    </h4>
+                ))}
 
             {!timer && (
-            <div className="d-flex flex-column justify-content-center align-items-center mt-3">
-                <Button
-                    key={"yes"}
-                    className="btn-primary"
-                    onClick={onNext}
-                    title={texts.next}
-                />
-            </div>
+                <div className="d-flex flex-column justify-content-center align-items-center mt-3">
+                    <Button
+                        key={"yes"}
+                        className="btn-primary"
+                        onClick={onNext}
+                        title={texts.next}
+                    />
+                </div>
             )}
 
             {last_song && (
