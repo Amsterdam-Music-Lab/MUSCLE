542bde4e8a04cff0f4ef8ea9e85182b644a403b3
Werner <werner@sudox.nl>
2022-07-14 10:35:26 +0200

    fix: fallback convert non-list to lists

--- backend/experiment/api/session.py
+++ backend/experiment/api/session.py
@@ -61,13 +61,18 @@ def create(request):
             'slug': experiment.slug}
         })
 
+    # convert non lists to list
+    next_round = session.experiment_rules().next_round(session)
+    if not isinstance(next_round, list): 
+        next_round = [next_round]
+
     data = {
         'session': {
             'id': session.id,
             'playlist': session.playlist.id,
             'json_data': session.load_json_data(),
         },
-        'next_round': [*session.experiment_rules().next_round(session)]
+        'next_round': next_round
     }
     return JsonResponse(data, json_dumps_params={'indent': 4})
 
