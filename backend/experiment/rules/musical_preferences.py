a1e9fe880e11f823599165e2428aabbb7ded0ebf
BeritJanssen <berit.janssen@gmail.com>
2022-08-23 18:12:39 +0200

    feat: compute and display best rated songs (#201)

--- backend/experiment/rules/musical_preferences.py
+++ backend/experiment/rules/musical_preferences.py
@@ -1,3 +1,5 @@
+import json
+
 from django.db.models import Avg
 from django.utils.translation import gettext_lazy as _
 
@@ -9,7 +11,6 @@ from .base import Base
 
 class MusicalPreferences(Base):
     ID = 'MUSICAL_PREFERENCES'
-    n_rounds = 8
 
     @classmethod
     def first_round(cls, experiment):
@@ -32,20 +33,26 @@ class MusicalPreferences(Base):
 
     @classmethod
     def next_round(cls, session, request_session=None):
-        round_number = session.get_next_round()
-        if round_number == cls.n_rounds:
+        n_results = session.rounds_passed()
+        if int(n_results / 2) == session.experiment.rounds:
             participant_results = session.result_set.filter(comment='like_song')
             participant_pref = cls.get_preferred_songs(participant_results)
-            all_results = []
-            for s in session.experiment.session_set.all():
-                if s.experiment.rules==cls.ID:
-                    all_results.extend(s.result_set.filter(comment='like_song'))
+            from experiment.models.result import Result 
+            all_results = [
+                ses.result_set.filter(comment='like_song') 
+                for ses in session.experiment.session_set.filter(
+                experiment__rules=cls.ID)
+            ]
+            all_results = Result.objects.filter(
+                comment='like_song'
+            )
             all_pref = cls.get_preferred_songs(all_results)
             view = Score(
                 session,
-                feedback = participant_pref
+                title='Preference overview',
+                feedback=json.dumps(all_pref)
             ).action()
-
+            return view
 
         section = session.playlist.random_section()
         result_id = cls.prepare_result(session, section)
@@ -88,13 +95,13 @@ class MusicalPreferences(Base):
             return None
     
     @classmethod
-    def get_preferred_songs(result_set, n=5):
+    def get_preferred_songs(cls, result_set, n=5):
         top_songs = result_set.values('section').annotate(
             avg_score=Avg('score')).order_by()[:n]
         from experiment.models.section import Section
         out_list = []
         for s in top_songs:
-            section = Section.objects.get(pk=s.section)
+            section = Section.objects.get(pk=s.get('section'))
             out_list.append({'artist': section.artist, 'name': section.name})
         return out_list
 
