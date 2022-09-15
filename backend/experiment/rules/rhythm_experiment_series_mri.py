7b6ed621d1df2728c2776f5cb111352e8d723f91
BeritJanssen <berit.janssen@gmail.com>
2022-09-15 09:12:34 +0200

    participant id not shown at the end of MRI version

--- backend/experiment/rules/rhythm_experiment_series_mri.py
+++ backend/experiment/rules/rhythm_experiment_series_mri.py
@@ -4,3 +4,4 @@ class RhythmExperimentSeriesMRI(RhythmExperimentSeries):
     ID = 'RHYTHM_SERIES_MRI'
     consent_form = 'consent/consent_MRI.html'
     debrief_form = 'final/debrief_MRI.html'
+    show_participant_final = False
