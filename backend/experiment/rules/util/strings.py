f633e6e2474a0824bc5161fe41e80f35c9e0802d
Werner <werner@sudox.nl>
2022-09-21 13:06:50 +0200

    feat: external_url generator

--- backend/experiment/rules/util/strings.py
+++ backend/experiment/rules/util/strings.py
@@ -1,5 +1,10 @@
-# Convert regular spaces to non breaking spacing 
-# on the given string
 def non_breaking(s):
-    non_breaking_space=chr(160)
+    # Convert regular spaces to non breaking spacing
+    # on the given string
+    non_breaking_space = chr(160)
     return s.replace(" ", non_breaking_space)
+
+
+def external_url(text, url):
+    # Create a HTML element for an external url
+    return '<a href="{}" target="_blank" rel="noopener noreferrer" >{}</a>'.format(url, text)
