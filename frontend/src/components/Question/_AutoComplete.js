8e5a1e916295b5435c888b5f83a82d8d7f81ca8e
Werner <werner@sudox.nl>
2022-09-22 16:16:51 +0200

    fix: ios scroll back to top after using select

--- frontend/src/components/Question/_AutoComplete.js
+++ frontend/src/components/Question/_AutoComplete.js
@@ -2,35 +2,35 @@ import React from "react";
 import classNames from "classnames";
 import Select from "react-select";
 
+// When using Safari iOS the browser may scroll the page when using the react-select Select.
+// Therefore we scroll back to top when the select is blurred
+const scrollToTop = () => {
+    window.scrollTo(0, 0);
+};
+
 // AutoComplete is a question view for selecting a single option from a dropdown list with autocompletion
-const AutoComplete = ({
-    question,
-    value,
-    onChange,
-    emphasizeTitle = false,
-}) => {
-    const options = Object.keys(question.choices).map((val, index) => ({
-        value: val,
-        label: question.choices[val],
-    })).sort((a,b)=>(a.label.localeCompare(b.label)));
+const AutoComplete = ({ question, value, onChange, emphasizeTitle = false }) => {
+    const options = Object.keys(question.choices)
+        .map((val, index) => ({
+            value: val,
+            label: question.choices[val],
+        }))
+        .sort((a, b) => a.label.localeCompare(b.label));
 
     return (
         <div className="aha__autocomplete">
-            {question.explainer && (
-                <p className="explainer">{question.explainer}</p>
-            )}
-            <h3 className={classNames({ title: emphasizeTitle })}>
-                {question.question}
-            </h3>
+            {question.explainer && <p className="explainer">{question.explainer}</p>}
+            <h3 className={classNames({ title: emphasizeTitle })}>{question.question}</h3>
             <div className="control">
                 <Select
                     options={options}
                     tabIndex="0"
                     name={question.key}
-                    value={options.find(option => option.value === value)}
+                    value={options.find((option) => option.value === value)}
                     onChange={(choice) => {
                         onChange(choice.value);
                     }}
+                    onBlur={scrollToTop}
                 />
             </div>
         </div>
