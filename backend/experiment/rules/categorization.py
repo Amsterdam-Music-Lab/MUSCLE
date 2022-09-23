2f175553ec3240bd21961beacb80710f5d31ab0b
Evert-R <evert.rot@gmx.com>
2022-09-23 13:00:59 +0200

    removed waiting time

--- backend/experiment/rules/categorization.py
+++ backend/experiment/rules/categorization.py
@@ -430,7 +430,6 @@ class Categorization(Base):
         trial.config['auto_advance_timer'] = 2500
         trial.config['style'] = json_data["button_order"]
         trial.config['time_pass_break'] = False
-        trial.playback.play_config['timeout_after_playback'] = 1750
 
         return trial.action()
 
