37264c62bc7736f4ab252b20aee078374bbbc668
BeritJanssen <berit.janssen@gmail.com>
2022-09-28 09:00:50 +0200

    add test function and setup

--- backend/experiment/tests/test_scoring.py
+++ backend/experiment/tests/test_scoring.py
@@ -1,4 +1,17 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+
+from experiment.models import Result, Session
 
 class ScoringTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.session = Session.objects.create()
     
+    def test_likert_score(self):
+        session_id = self.session.pk
+        client_request = {
+            
+        }
+        
+
