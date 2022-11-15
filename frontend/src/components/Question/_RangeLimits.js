da8a168c00a63fd475c27bbe0440d24772ebb4f7
BeritJanssen <berit.janssen@gmail.com>
2022-11-15 10:41:18 +0100

    feat: factor out RangeLimits and RangeTitle, option to display all labels under Slider

---
+++ frontend/src/components/Question/_RangeLimits.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+
+import { renderLabel } from "../../util/label";
+
+const RangeLimits = ({ minVal, maxVal, labels}) => {
+    return (
+        <div className="limits">
+            { labels ? 
+                <div className="labels">
+                    {labels.map(label => (
+                        <span>{renderLabel(label)}</span>
+                    )
+                    )}                
+                </div> :
+                <div>
+                    <span className="min">{renderLabel(minVal)}</span>
+                    <span className="max">{renderLabel(maxVal)}</span>
+                </div>
+            }
+        </div>
+    )
+};
+
+export default RangeLimits;
