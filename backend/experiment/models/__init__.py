70771812c46bc4544bc0a78033b9bc338faf37da
BeritJanssen <berit.janssen@gmail.com>
2023-01-09 10:41:37 +0100

    delete profile model; add migration

--- backend/experiment/models/__init__.py
+++ backend/experiment/models/__init__.py
@@ -3,9 +3,8 @@ from .experiment_series import ExperimentSeries
 from .section import Section
 from .experiment import Experiment
 from .participant import Participant
-from .profile import Profile
 from .session import Session
 from .result import Result
 
 __all__ = ["Playlist", "Section", "Experiment",
-           "Participant", "Profile", "Session", "Result", "ExperimentSeries"]
+           "Participant", "Session", "Result", "ExperimentSeries"]
