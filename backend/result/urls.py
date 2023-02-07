be6b95b9d9ee958bd855b6ecc3bba8f71c012161
BeritJanssen <berit.janssen@gmail.com>
2023-02-07 08:22:24 +0100

    change name: result/create -> result/score

--- backend/result/urls.py
+++ backend/result/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from result.views import current_profile, create, consent, get_result
+from result.views import current_profile, consent, get_result, score
 
 app_name = 'result'
 
 urlpatterns = [
      path('current_profile/', current_profile,
          name='current_profile'),
-     path('create/', create,
-         name='result_create'),
+     path('score/', score,
+         name='result_score'),
      path('consent/', consent,
           name='register_consent'),
      path('<slug:question>/', get_result,
