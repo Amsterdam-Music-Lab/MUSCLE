e45079f530f4443823358828d810ccc5506784e1
Evert-R <evert.rot@gmx.com>
2023-02-16 14:47:07 +0100

    determine server host

--- selenium/config/init_selenium.py
+++ selenium/config/init_selenium.py
@@ -2,16 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+debug = bool(os.getenv("AML_DEBUG"))
+api_root = os.getenv("API_ROOT")
 
 
 class InitSelenium():
     """Init Selenium
 
-    attributes: driver    
+    attributes: driver, host
     """
+
     driver = None
+    host = None
 
     def setup_chrome(self):
+        """Initialize chrome driver        
+        """
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
@@ -19,3 +27,13 @@ class InitSelenium():
         self.driver = webdriver.Chrome(service=Service(
             ChromeDriverManager().install()), options=chrome_options)
         return self.driver
+
+    def get_host(self):
+        """Determine server host name for selenium
+        """
+        if debug is True:
+            # use docker service host when running locally
+            self.host = 'http://client:3000'
+        else:
+            self.host = api_root
+        return self.host
