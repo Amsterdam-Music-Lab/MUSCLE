cc1c16697f8e155260a59d879087d4116b7912a8
Evert-R <evert.rot@gmx.com>
2023-02-16 14:48:07 +0100

    use init host and custom slug

--- selenium/example_script.py
+++ selenium/example_script.py
@@ -7,14 +7,15 @@ import time
 # Run this script from the terminal:
 # docker-compose run selenium bash -c "python example_script.py"
 
+# slug of the experiment to run this script on
+SLUG = 'cat'
+# Determine current server host name
+HOST = InitSelenium().get_host()
 # initialize webdriver
 driver = InitSelenium().setup_chrome()
 
 try:
-    driver.get('http://client:3000/cat')
-
-    for entry in driver.get_log('browser'):
-        print(entry)
+    driver.get(HOST + '/' + SLUG)
     time.sleep(1)
     element = driver.find_element(By.TAG_NAME, "body")
     print(element.text)
