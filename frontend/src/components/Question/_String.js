0f8edbb4c78668687f60d31ea8cc254183a1cb45
Evert-R <evert.rot@gmx.com>
2023-03-07 11:00:46 +0100

    Validation for text and numbers

--- frontend/src/components/Question/_String.js
+++ frontend/src/components/Question/_String.js
@@ -4,9 +4,32 @@ import React, { useRef, useEffect } from "react";
 const String = ({ question, value = "", onChange }) => {
     const input = useRef(null);
 
+    // Input validation
     const handleChange = () => {
-        onChange(input.current.value);
+        switch (question.input_type) {
+            case 'number':
+                if (input.current.value >= question.min_value && input.current.value <= question.max_value) {
+                    onChange(input.current.value);
+                }
+                break;
+            case 'text':
+                if (question.max_length && input.current.value.length > question.max_length) {
+                    break;
+                }
+                onChange(input.current.value);
+                break;
+            default:
+                onChange(input.current.value);
+        }
     };
+
+    // Key validation
+    const handleKey = (e) => {
+        console.log(e.charCode)
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    }
 
     useEffect(() => {
         if (input.current) {
@@ -17,12 +40,11 @@ const String = ({ question, value = "", onChange }) => {
     return (
         <div className="aha__string">
             <input
-                type="text"
+                type={question.input_type}
                 value={value}
                 ref={input}
-                maxLength={question.max_length}
                 onChange={handleChange}
-                onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+                onKeyPress={(e) => { handleKey(e) }}                
             />
         </div>
     );
