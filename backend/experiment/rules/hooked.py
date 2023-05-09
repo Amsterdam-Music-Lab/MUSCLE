9374c46d998cf5659e59950b7a5a020d30f7087e
BeritJanssen <berit.janssen@gmail.com>
2023-05-09 13:17:47 +0200

    move plan_sections logic to hooked base class

--- backend/experiment/rules/hooked.py
+++ backend/experiment/rules/hooked.py
@@ -1,18 +1,24 @@
-from django.conf import settings
+import random
+import logging
 
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.template.loader import render_to_string
 
 from .base import Base
-from experiment.actions import  Explainer, Consent, StartSession, Final, Score, Playlist, Step, Trial
-from experiment.actions.form import Form
-
+from experiment.actions import  Consent, Explainer, Final, Playlist, Score, SongSync, StartSession, Step, Trial
+from experiment.actions.form import BooleanQuestion, Form
+from experiment.actions.playback import Playback
 from experiment.questions.demographics import DEMOGRAPHICS
 from experiment.questions.utils import unasked_question
 from experiment.questions.goldsmiths import MSI_FG_GENERAL, MSI_ALL
 from experiment.questions.stomp import STOMP20
 from experiment.questions.tipi import TIPI
+from experiment.actions.styles import STYLE_BOOLEAN_NEGATIVE_FIRST
 from experiment.actions.utils import combine_actions
+from result.utils import prepare_result
+
+logger = logging.getLogger(__name__)
 
 
 class Hooked(Base):
@@ -21,6 +27,7 @@ class Hooked(Base):
     consent_file = 'consent_hooked.html';
     timeout = 15
     questions = True
+    round_modifier = 0
 
     @classmethod
     def first_round(cls, experiment):
@@ -125,6 +132,7 @@ class Hooked(Base):
     @classmethod
     def next_round(cls, session):
         """Get action data for the next round"""
+        json_data = session.load_json_data()
 
         # If the number of results equals the number of experiment.rounds,
         # close the session and return data for the final_score view.
@@ -176,12 +184,8 @@ class Hooked(Base):
             ).action())
 
             # Load the heard_before offset.
-            try:
-                heard_before_offset = \
-                    session.load_json_data()['plan']['n_song_sync'] + 1
-            except KeyError as error:
-                print('Missing plan key: %s' % str(error))
-                return actions
+            plan = json_data.get('plan')
+            heard_before_offset = len(plan['song_sync_sections']) + 1
 
             # SongSync rounds. Skip questions until Round 5.
             if next_round_number in range(2, 5):
@@ -254,5 +258,134 @@ class Hooked(Base):
         return score_message + " " + song_sync_message + " " + heard_before_message
 
     @classmethod
-    def get_trial_title(cls, session, next_round_number):
-        return _('Round {} / {}').format(next_round_number, session.experiment.rounds)
+    def get_trial_title(cls, session, next_round_number):    
+        return _("Round %(number)d / %(total)d") %\
+            {'number': next_round_number, 'total': session.experiment.rounds}
+    
+    @classmethod
+    def plan_sections(cls, session):
+        """Set the plan of tracks for a session.
+        """
+
+        # Which songs are available?
+        songs = list(session.playlist.song_ids())
+        random.shuffle(songs)
+
+        # How many sections do we need?
+        # 2/3 of the rounds are SongSync, of which 1/4 old songs, 3/4 "free"
+        # 1/3 of the rounds are "heard before", of which 1/2 old songs
+        # e.g. 30 rounds -> 20 SongSync with 5 songs to be repeated later
+        n_rounds = session.experiment.rounds
+        n_old = round(0.17 * n_rounds)
+        n_new = round(0.17 * n_rounds)
+        n_free = n_rounds - 2 * n_old - n_new
+
+        # Assign songs.
+        old_songs = songs[:n_old]
+        new_songs = songs[n_old:n_old+n_new]
+        free_songs = songs[n_old+n_new:n_old+n_new+n_free]
+
+        # Assign sections.
+        old_sections = [session.section_from_song(s) for s in old_songs]
+        free_sections = [session.section_from_song(s) for s in free_songs]
+        new_sections = [session.section_from_song(s) for s in new_songs]
+
+        # Randomise.
+        old_section_info = [{'novelty': 'old', 'id': s.id}
+                            for s in old_sections]
+        song_sync_sections = old_section_info + [
+            {'novelty': 'free', 'id': s.id} for s in free_sections]
+        random.shuffle(song_sync_sections)
+        heard_before_sections = old_section_info + [
+            {'novelty': 'new', 'id': s.id} for s in new_sections]
+        random.shuffle(heard_before_sections)
+        plan = {
+            'song_sync_sections': song_sync_sections,
+            'heard_before_sections': heard_before_sections
+        }
+
+        # Save, overwriting existing plan if one exists.
+        session.save_json_data({'plan': plan})
+
+    @classmethod
+    def next_song_sync_action(cls, session):
+        """Get next song_sync section for this session."""
+
+        # Load plan.
+        next_round_number = session.get_current_round() - cls.round_modifier
+        try:
+            plan = session.load_json_data()['plan']
+            sections = plan['song_sync_sections']
+        except KeyError as error:
+            logger.error('Missing plan key: %s' % str(error))
+            return None
+
+        # Get section.
+        section = None
+        if next_round_number <= len(sections):
+            section = \
+                session.section_from_any_song(
+                    {'id': sections[next_round_number-1].get('id')})
+        if not section:
+            logger.warning("Warning: no next_song_sync section found")
+            section = session.section_from_any_song()
+        key = 'song_sync'
+        result_id = prepare_result(key, session, section=section, scoring_rule='SONG_SYNC')
+        return SongSync(
+            section=section,
+            title=cls.get_trial_title(session, next_round_number),
+            key=key,
+            result_id=result_id
+        ).action()
+    
+    @classmethod
+    def next_heard_before_action(cls, session):
+        """Get next heard_before action for this session."""
+
+        # Load plan.
+        next_round_number = session.get_current_round() - cls.round_modifier
+        try:
+            plan = session.load_json_data()['plan']
+            sections = plan['heard_before_sections']
+            heard_before_offset = len(plan['song_sync_sections']) + 1
+        except KeyError as error:
+            logger.error('Missing plan key: %s' % str(error))
+            return None
+        # Get section.
+        section = None
+        if next_round_number - heard_before_offset  <= len(sections):
+            this_section_info = sections[next_round_number - heard_before_offset]
+            section = session.section_from_any_song(
+                    {'id': this_section_info.get('id')})
+        if not section:
+            logger.warning("Warning: no heard_before section found")
+            section = session.section_from_any_song()
+        playback = Playback(
+            [section],
+            play_config={'ready_time': 3, 'show_animation': True},
+            preload_message=_('Get ready!'))
+        expected_response = this_section_info.get('novelty')
+        # create Result object and save expected result to database
+        key = 'heard_before'
+        form = Form([BooleanQuestion(
+            key=key,
+            choices={
+                'new': _("NO"),
+                'old': _("YES"),
+            },
+            question=_("Did you hear this song in previous rounds?"),
+            result_id=prepare_result(key, session, section=section, expected_response=expected_response, scoring_rule='REACTION_TIME',),
+            submits=True,
+            style={STYLE_BOOLEAN_NEGATIVE_FIRST: True, 'buttons-large-gap': True})
+            ])
+        config = {
+            'auto_advance': True,
+            'response_time': cls.timeout
+        }
+        trial = Trial(
+            title=cls.get_trial_title(session, next_round_number),
+            playback=playback,
+            feedback_form=form,
+            config=config,
+        )
+        return trial.action()
