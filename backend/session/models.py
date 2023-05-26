0f77f975c5bf6d2721d004794d68cc205ddc3501
BeritJanssen <berit.janssen@gmail.com>
2023-05-26 14:48:31 +0200

    fix song.restricted logic in playlist.filter_songs

--- backend/session/models.py
+++ backend/session/models.py
@@ -120,7 +120,7 @@ class Session(models.Model):
             **(
                 {}
                 if self.participant.is_dutch()
-                else {'song_restricted': []}
+                else {'song.restricted': []}
             ),
             **filter_by
         ).values_list('song_id', flat=True)
