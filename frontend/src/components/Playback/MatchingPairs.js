49e0d2179ff83e3c475186350251d3a3a0251c25
Evert-R <evert.rot@gmx.com>
2023-06-18 16:48:09 +0200

    reworked turn logic to three clicks

--- frontend/src/components/Playback/MatchingPairs.js
+++ frontend/src/components/Playback/MatchingPairs.js
@@ -16,6 +16,7 @@ const MatchingPairs = ({
     const yPosition = useRef(-1);
     const score = useRef(undefined);
     const firstCard = useRef(-1);
+    const secondCard = useRef(-1);
     const [total, setTotal] = useState(100);
     const [message, setMessage] = useState('')
     
@@ -47,35 +48,22 @@ const MatchingPairs = ({
 
     // Show (animated) feedback after second click on second card or finished playing
     const showFeedback = () => {        
-        finishedPlaying();        
+                
         const turnedCards = sections.filter(s => s.turned);
         // Check if this turn has finished
         if (turnedCards.length === 2) {                        
             // update total score & display current score
             setTotal(calculateRunningScore());
-            setMessage(setScoreMessage(score.current));
-            // Turn all cards back and enable events after animations have finished
-            setTimeout(() => {
-                score.current = undefined;
-                sections.forEach(section => section.turned = false);
-                sections.forEach(section => section.noevents = false);
-                firstCard.current = -1;
-                setMessage('<br/> Try again');
-                setFeedback(false);                
-            }, finishDelay);
+            setMessage(setScoreMessage(score.current));            
             // show end of turn animations
             switch (score.current) {                                       
                 case 1:
                     turnedCards[0].lucky = true;
-                    turnedCards[1].lucky = true;
-                    turnedCards[0].inactive = true;
-                    turnedCards[1].inactive = true;                    
+                    turnedCards[1].lucky = true;                                        
                     break;
                 case 2:
                     turnedCards[0].memory = true;
-                    turnedCards[1].memory = true;                    
-                    turnedCards[0].inactive = true;
-                    turnedCards[1].inactive = true;                    
+                    turnedCards[1].memory = true;                                        
                     break;
                 default:
                     turnedCards[0].nomatch = true;
@@ -84,18 +72,13 @@ const MatchingPairs = ({
                     setTimeout(() => {
                         turnedCards[0].nomatch = false;
                         turnedCards[1].nomatch = false;                        
-                      }, 700);
+                      }, finishDelay);
                     break;  
             }   
             // Update the component to show animations 
             setFeedback(true);
-            // Check if the board is empty
-            if (sections.filter(s => s.inactive).length === sections.length) {
-                // all cards have been turned
-                setTimeout(() => {
-                    submitResult({moves: resultBuffer.current});
-                  }, finishDelay);            
-            }        
+            // add third click event to finish the turn
+            document.getElementById('root').addEventListener('click', finishTurn);
             return;
         }
     }
@@ -106,10 +89,11 @@ const MatchingPairs = ({
         if (turnedCards.length < 2) {
             if (turnedCards.length === 1) {
                 // We have two turned cards
-                currentCard.turned = true;                
+                currentCard.turned = true;
+                secondCard.current = index;                
                 // set no mouse events for all but current
                 sections.forEach(section => section.noevents = true);                
-                currentCard.noevents = false;
+                currentCard.noevents = true;
                 // check for match
                 const lastCard = sections[firstCard.current];                
                 if (lastCard.group === currentCard.group) {
@@ -124,7 +108,8 @@ const MatchingPairs = ({
                     else { score.current = 0; }
                 };
                 currentCard.seen = true;
-                lastCard.seen = true;                
+                lastCard.seen = true;
+                showFeedback();
             } else {
                 // first click of the turn
                 firstCard.current = index;
@@ -141,12 +126,38 @@ const MatchingPairs = ({
                 score: score.current,
                 timestamp: formatTime(Date.now() - startTime.current)
             });
-            
-        } else {
-            // second click on second card
-            showFeedback();            
         }
+        return;
     };
+
+    const finishTurn = () => {
+        finishedPlaying();
+        // remove matched cards from the board
+        if (score.current === 1 || score.current === 2) {            
+            sections[firstCard.current].inactive = true;
+            sections[secondCard.current].inactive = true;            
+        } else {
+            // reset nomatch state
+            sections[firstCard.current].nomatch = false;
+            sections[secondCard.current].nomatch = false;            
+        }
+        firstCard.current = -1;
+        secondCard.current = -1;
+        // remove third click event
+        document.getElementById('root').removeEventListener('click', finishTurn);
+        score.current = undefined;
+        // Turn all cards back and enable events
+        sections.forEach(section => section.turned = false);
+        sections.forEach(section => section.noevents = false);        
+        // Check if the board is empty
+        if (sections.filter(s => s.inactive).length === sections.length) {
+            // all cards have been turned
+            setTimeout(() => {
+                submitResult({moves: resultBuffer.current});
+              }, finishDelay);            
+        } else { setMessage('<br/> Try again'); }         
+        setFeedback(false);                        
+    }
 
     const calculateRunningScore = () => {        
         const allScores = resultBuffer.current.filter(
