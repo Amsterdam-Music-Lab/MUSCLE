85828db4f78e919402f655bab05e22318e497463
BeritJanssen <berit.janssen@gmail.com>
2023-07-04 13:20:43 +0200

    adjust audio check countdown hooked-china to 15s

--- backend/experiment/rules/huang_2022.py
+++ backend/experiment/rules/huang_2022.py
@@ -109,7 +109,7 @@ class Huang2022(Hooked):
                 submits=True,
                 style=STYLE_BOOLEAN_NEGATIVE_FIRST)])
             return Trial(playback=playback, feedback_form=form, html=html,
-                         config={'response_time': 120},
+                         config={'response_time': 15},
                          title=_("Audio check")).action()
         elif next_round_number <= 1:
             last_result = session.result_set.last()
@@ -125,7 +125,7 @@ class Huang2022(Hooked):
                         style=STYLE_BOOLEAN_NEGATIVE_FIRST
                     )])
                     return Trial(playback=playback, html=html, feedback_form=form,
-                                 config={'response_time': 120},
+                                 config={'response_time': 15},
                                  title=_("Ready to experiment")).action()
                 else:
                     session.increment_round() # adjust round numbering
