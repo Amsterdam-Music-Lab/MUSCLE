91329a38d3b50dc175375a5e2249312ebdc31b3f
BeritJanssen <berit.janssen@gmail.com>
2023-08-07 13:56:42 +0200

    longer section duration in rhythm_discrimination

--- backend/experiment/rules/rhythm_discrimination.py
+++ backend/experiment/rules/rhythm_discrimination.py
@@ -187,7 +187,7 @@ def next_trial_actions(session, round_number, request_session):
         title=_('Rhythm discrimination: %s' % (title)),
         config={
             'listen_first': True,
-            'response_time': section.duration + .1
+            'response_time': section.duration + .5
         }
     )
 
