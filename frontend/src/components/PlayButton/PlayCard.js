54263fac9a3d09b525f2c8b91bfe9968c7773542
Evert-R <evert.rot@gmx.com>
2023-08-16 22:37:20 +0200

    Only create buffers on first load

--- frontend/src/components/PlayButton/PlayCard.js
+++ frontend/src/components/PlayButton/PlayCard.js
@@ -1,25 +1,36 @@
+import React, {useEffect, useState} from "react";
 import classNames from "classnames";
 
 import Histogram from "../Histogram/Histogram";
 import { MEDIA_ROOT } from "../../config";
 const PlayCard = ({ onClick, registerUserClicks, playing, section }) => {
+    const [firstRender, setFirstRender] = useState(false);
 
-    // preload the section (temporary solution for ICMPC)
-    const audioBuffer = document.createElement("audio");
-    audioBuffer.id = `buffer-${section.id}`;    
-    audioBuffer.src = MEDIA_ROOT + section.url;
-    audioBuffer.crossorigin = "use-credentials";
-    audioBuffer.disableRemotePlayback = true;
-    audioBuffer.style.display = "none";
+    useEffect(() => {
+        if (!firstRender) {
+          createBuffer();
+          setFirstRender(true);
+        }
+    }, [firstRender])
+    
+    const createBuffer = () => {
+        // preload the section (temporary solution for ICMPC)
+        const audioBuffer = document.createElement("audio");
+        audioBuffer.id = `buffer-${section.id}`;    
+        audioBuffer.src = MEDIA_ROOT + section.url;
+        audioBuffer.crossorigin = "use-credentials";
+        audioBuffer.disableRemotePlayback = true;
+        audioBuffer.style.display = "none";
 
-    // Required for Firefox to trigger canplaythrough event
-    audioBuffer.preload = "auto";
+        // Required for Firefox to trigger canplaythrough event
+        audioBuffer.preload = "auto";
 
-    // Required for Safari/iOS
-    audioBuffer.load();
+        // Required for Safari/iOS
+        audioBuffer.load();
 
-    // Add it to the page
-    document.body.appendChild(audioBuffer);
+        // Add it to the page
+        document.body.appendChild(audioBuffer);
+    }
     
     return (
         <div className={classNames("aha__play-card", {turned: section.turned}, {noevents: section.noevents}, {disabled: section.inactive}, { memory: section.memory }, { lucky: section.lucky }, { nomatch: section.nomatch })} onClick={event => {
