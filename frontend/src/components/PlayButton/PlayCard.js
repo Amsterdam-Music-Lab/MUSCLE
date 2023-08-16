51feab68e4039bc2527c2f63ead36432e380bd19
Evert-R <evert.rot@gmx.com>
2023-08-16 22:09:42 +0200

    preload the section of each playcard

--- frontend/src/components/PlayButton/PlayCard.js
+++ frontend/src/components/PlayButton/PlayCard.js
@@ -1,8 +1,25 @@
 import classNames from "classnames";
 
 import Histogram from "../Histogram/Histogram";
-
+import { MEDIA_ROOT } from "../../config";
 const PlayCard = ({ onClick, registerUserClicks, playing, section }) => {
+
+    // preload the section (temporary solution for ICMPC)
+    const audioBuffer = document.createElement("audio");
+    audioBuffer.id = `buffer-${section.id}`;    
+    audioBuffer.src = MEDIA_ROOT + section.url;
+    audioBuffer.crossorigin = "use-credentials";
+    audioBuffer.disableRemotePlayback = true;
+    audioBuffer.style.display = "none";
+
+    // Required for Firefox to trigger canplaythrough event
+    audioBuffer.preload = "auto";
+
+    // Required for Safari/iOS
+    audioBuffer.load();
+
+    // Add it to the page
+    document.body.appendChild(audioBuffer);
     
     return (
         <div className={classNames("aha__play-card", {turned: section.turned}, {noevents: section.noevents}, {disabled: section.inactive}, { memory: section.memory }, { lucky: section.lucky }, { nomatch: section.nomatch })} onClick={event => {
@@ -21,7 +38,7 @@ const PlayCard = ({ onClick, registerUserClicks, playing, section }) => {
                     :
                     <div className={classNames("back", {seen: section.seen})}>
                     </div>
-                }
+            }            
         </div>
     );
 };
