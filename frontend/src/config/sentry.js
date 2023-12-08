538754ce13a2cadb2b972cf6990ab25aaff318b5
Drikus Roor <drikusroor@gmail.com>
2023-12-08 09:50:50 +0100

    config: Initialize and document Sentry with DSN key from env variable

--- frontend/src/config/sentry.js
+++ frontend/src/config/sentry.js
@@ -1,10 +1,17 @@
 //...
 import * as Sentry from "@sentry/react";
 
+const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN;
 
-export const initSentry = () =>
-    Sentry.init({
-        dsn: "https://21d71c12f155fb5aae96a480ed35c473@o4506343330021376.ingest.sentry.io/4506343351320576",
+export const initSentry = () => {
+
+    if (!SENTRY_DSN) {
+        console.warn("Sentry DSN not found. Sentry will not be initialized.");
+        return;
+    }
+
+    return Sentry.init({
+        dsn: SENTRY_DSN,
         integrations: [
             new Sentry.BrowserTracing({
                 // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
@@ -18,5 +25,6 @@ export const initSentry = () =>
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
+}
 
 export default initSentry;
