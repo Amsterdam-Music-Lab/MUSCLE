bd5000210504acf140b2d920f38e5aa6cfeef5c0
Drikus Roor <drikusroor@gmail.com>
2023-12-11 14:24:06 +0100

    fix: Fix absolute import issue (#650)

--- frontend/src/components/CountDown/CountDown.js
+++ frontend/src/components/CountDown/CountDown.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Timer from "../../util/timer";
+import Timer from "util/timer";
 import classNames from "classnames";
 
 // CountDown to zero
