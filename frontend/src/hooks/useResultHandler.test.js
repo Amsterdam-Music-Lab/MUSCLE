620200a22520bc7ebab8da6a19f937260c72cbdd
Drikus Roor <drikusroor@gmail.com>
2023-12-13 16:15:42 +0100

    refactor: Remove unnecessary testing library dependencies as per React 18
    This commit removes unnecessary testing library dependencies from the frontend package.json and useResultHandler.test.js files. By removing the @testing-library/react-hooks dependency, the codebase is simplified and optimized.

--- frontend/src/hooks/useResultHandler.test.js
+++ frontend/src/hooks/useResultHandler.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react";
 import useResultHandler from "./useResultHandler";
 
 import * as API from '../API.js';
