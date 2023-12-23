baa893e28e58d705c28393834506f32adf76fb0e
Drikus Roor <drikusroor@gmail.com>
2023-12-23 23:06:50 +0100

    config: Update import paths for Timer module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,20 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
     base: '',
     plugins: [react(), viteTsconfigPaths()],
     server: {
-        // this ensures that the browser opens upon server start
         open: true,
-        // this sets a default port to 3000
         port: 3000,
+    },
+    esbuild: {
+        include: /\.[jt]sx?$/,
+        exclude: [],
+        loader: 'jsx',
+    },
+    resolve: {
+        alias: {
+            '@/': '/src/',
+        },
     },
 })
