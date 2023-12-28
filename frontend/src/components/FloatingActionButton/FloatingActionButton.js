849ed3114282e79c0e058796be4e6d1514ff420b
Drikus Roor <drikusroor@gmail.com>
2023-12-28 13:53:28 +0100

    test: Refactor FloatingActionButton component and add unit tests

--- frontend/src/components/FloatingActionButton/FloatingActionButton.js
+++ frontend/src/components/FloatingActionButton/FloatingActionButton.js
@@ -21,27 +21,39 @@ const FloatingActionButton = ({
     }
 
     return (<>
-        <div className={
-            classNames("floating-action-button",
-                getPositionClassNames(position),
-                expanded && 'floating-action-button--expanded',
-                className
-            )}>
+        <div
+            data-testid="floating-action-button"
+            className={
+                classNames("floating-action-button",
+                    getPositionClassNames(position),
+                    expanded && 'floating-action-button--expanded',
+                    className
+                )}
+        >
             <button
+                data-testid="floating-action-button__toggle-button"
                 className='floating-action-button__toggle-button'
                 onClick={() => setExpanded(!expanded)}
             >
-                {expanded ? <i className={`floating-action-button__icon fa fa-times`}></i> : <i className={`floating-action-button__icon fa ${icon}`}></i>}
+                <i
+                    data-testid="floating-action-button__icon"
+                    className={`floating-action-button__icon fa ${expanded ? 'fa-times' : icon}`}
+                />
             </button>
-            <div className='floating-action-button__content'>
+            <div
+                data-testid="floating-action-button__content"
+                className='floating-action-button__content'
+            >
                 {children}
             </div>
         </div>
-        <div className={
-            classNames(
-                'floating-action-button__overlay',
-                expanded && 'floating-action-button__overlay--expanded'
-            )}
+        <div
+            data-testid="floating-action-button__overlay"
+            className={
+                classNames(
+                    'floating-action-button__overlay',
+                    expanded && 'floating-action-button__overlay--expanded'
+                )}
             onClick={() => setExpanded(false)}
             aria-hidden={expanded ? 'false' : 'true'}
             role="presentation"
