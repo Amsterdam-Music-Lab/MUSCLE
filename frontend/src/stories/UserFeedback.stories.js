97c830f438b14733e2159ee001986272d669f748
Drikus Roor <drikusroor@gmail.com>
2023-12-28 11:45:29 +0100

    style: Refactor UserFeedback component and styles to include vertical layout

--- frontend/src/stories/UserFeedback.stories.js
+++ frontend/src/stories/UserFeedback.stories.js
@@ -17,7 +17,29 @@ export const Default = {
             button: 'Submit',
             thank_you: 'Thank you for your feedback!',
             contact_body: '<p>Please contact us at <a href="mailto:info@example.com">info@example.com</a> if you have any questions.</p>'
-        }
+        },
+        inline: true
+    },
+    decorators: [
+        (Story) => (
+            <div style={{ width: '100%', height: '100%', backgroundColor: '#fff', padding: '3rem' }}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+
+export const Vertical = {
+    args: {
+        experimentSlug: 'test',
+        participant: 'test',
+        feedbackInfo: {
+            header: 'Feedback',
+            button: 'Submit',
+            thank_you: 'Thank you for your feedback!',
+            contact_body: '<p>Please contact us at <a href="mailto:info@example.com">info@example.com</a> if you have any questions.</p>'
+        },
+        inline: false
     },
     decorators: [
         (Story) => (
