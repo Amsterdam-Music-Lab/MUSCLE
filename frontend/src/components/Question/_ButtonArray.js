2b6828f2d47e1f23284832606781bf1b8562f6da
Drikus Roor <drikusroor@gmail.com>
2024-01-19 18:11:56 +0100

    Fixed: Fix one curly bracket too many (#723)

--- frontend/src/components/Question/_ButtonArray.js
+++ frontend/src/components/Question/_ButtonArray.js
@@ -52,10 +52,8 @@ const ToggleButton = ({ label, value, index, name, disabled, onChange, checked }
                 checked={checked}
                 aria-checked={checked}
                 disabled={disabled}
-                onChange={() => onChange(value)}}
-                onKeyPress={() => {
-                    onChange(value);
-                }}
+                onChange={() => onChange(value)}
+                onKeyPress={() => onChange(value)}
             />
             {renderLabel(label)}
         </label>)
