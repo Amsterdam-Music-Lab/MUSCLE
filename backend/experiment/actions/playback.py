33d4ee4a7f60d7c8e8c0c1c90a38266f2eb32b69
Drikus Roor <drikusroor@gmail.com>
2024-01-23 09:26:28 +0100

    fix: Fix score feedback display in MatchingPairs class

--- backend/experiment/actions/playback.py
+++ backend/experiment/actions/playback.py
@@ -130,7 +130,7 @@ class MatchingPairs(Multiplayer):
     play buttons are represented as cards
     '''
 
-    def __init__(self, sections: List[Dict], score_feedback_display: str = 'large_top', **kwargs):
+    def __init__(self, sections: List[Dict], score_feedback_display: str = 'large-top', **kwargs):
         super().__init__(sections, **kwargs)
         self.ID = TYPE_MATCHINGPAIRS
         self.score_feedback_display = score_feedback_display
