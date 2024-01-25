b317b280b075a7f7f5db44168ba8c8d0f7566b01
Drikus Roor <drikusroor@gmail.com>
2024-01-25 17:36:42 +0100

    feat: Add test_experiment template

---
+++ backend/experiment/management/commands/templates/test_experiment.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from experiment.models import Experiment
+from participant.models import Participant
+from section.models import Playlist
+from session.models import Session
+
+class NewExperimentTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        self.participant = Participant.objects.create()
+        self.playlist = Playlist.objects.create(name='NewExperiment')
+        self.experiment = Experiment.objects.create(name='NewExperiment', rounds=5)
+        self.session = Session.objects.create(
+            experiment=self.experiment,
+            participant=self.participant,
+            playlist=self.playlist
+        )
+
+    def test_initializes_correctly(self):
+        assert self.experiment.name == 'NewExperiment'
