4cbecd1ebfb861b1cb4f98a92a01f0459e4e9bb8
Evert-R <evert.rot@gmx.com>
2024-02-06 11:32:00 +0100

    add django markup for consent

--- backend/aml/base_settings.py
+++ backend/aml/base_settings.py
@@ -14,6 +14,7 @@ import os
 from corsheaders.defaults import default_headers
 import sentry_sdk
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inline_actions',
+    'django_markup',
     'corsheaders',
     'experiment',
     'participant',
@@ -176,3 +178,9 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     profiles_sample_rate=0.2,
 )
+
+MARKUP_SETTINGS = {
+    'markdown': {
+        'safe_mode': True
+    }
+}
