a229199420bc3aab3dfbb986df6547bec4471379
Evert-R <evert.rot@gmx.com>
2024-02-13 10:35:03 +0100

    test no markup rendering

--- backend/experiment/tests/test_actions_consent.py
+++ backend/experiment/tests/test_actions_consent.py
@@ -8,3 +8,7 @@ class ConsentTest(TestCase):
     def test_markdown_rendering(self):
         consent = Consent('#test', render_format='MARKDOWN')
         self.assertEqual(consent.text, '<h1>test</h1>')
+
+    def test_no_markdown_rendering(self):
+        consent = Consent('#test')
+        self.assertEqual(consent.text, '#test')
