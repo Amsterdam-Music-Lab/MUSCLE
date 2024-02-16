17c9bca62f4a3abfa65d59137e3d7ef4c8545cb2
Drikus Roor <drikusroor@gmail.com>
2024-02-16 13:21:18 +0100

    feat: Add preview functionality for font, logo, and background images in the theme overview

--- backend/theme/admin.py
+++ backend/theme/admin.py
@@ -1,11 +1,16 @@
+import re
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import ThemeConfig
 from .forms import ThemeConfigForm
 
 
 @admin.register(ThemeConfig)
 class ThemeConfigAdmin(admin.ModelAdmin):
+
     form = ThemeConfigForm
+
     fieldsets = (
         ('Theme Configuration', {
             'fields': ('name',)
@@ -38,4 +43,52 @@ class ThemeConfigAdmin(admin.ModelAdmin):
             'fields': ('global_css',)
         }),
     )
-    list_display = ['id']
+
+    list_display = ('id', 'name_link', 'font_preview', 'logo_preview', 'background_preview')
+
+    def name_link(self, obj):
+        return format_html('<a href="{}">{}</a>', f'/admin/theme/themeconfig/{obj.id}/change/', obj.name)
+
+    def font_preview(self, obj):
+        if obj.font:
+            # Check if the font field contains a URL
+            if obj.font.startswith('http://') or obj.font.startswith('https://'):
+                # Font is specified as a URL
+
+                # Extract font-family name from URL
+                font_family_match = re.search(r'family=([^&:]+)', obj.font)
+                font_family = font_family_match.group(1).replace("+", " ") if font_family_match else "sans-serif"
+
+                return format_html(
+                    '<link href="{font_url}" rel="stylesheet">'
+                    '<div style="font-family: \'{font_family}\'; font-size: 16px;">Preview Text</div>',
+                    font_url=obj.font,
+                    font_family=font_family
+                )
+            else:
+                # Font is specified as a Google Font name
+                font_name = obj.font.replace(" ", "+")  # Replace spaces with '+' for the URL
+                return format_html(
+                    '<link href="https://fonts.googleapis.com/css2?family={font_name}&display=swap" rel="stylesheet">'
+                    '<div style="font-family: \'{font}\'; font-size: 16px;">Preview Text</div>',
+                    font_name=font_name,
+                    font=obj.font
+                )
+        return "No font selected"
+
+    def logo_preview(self, obj):
+        if obj.logo:
+            return mark_safe(f'<img src="{obj.logo}" style="max-height: 50px;"/>')
+        return ""
+
+    def background_preview(self, obj):
+        if obj.background:
+            return mark_safe(f'<div style="background-image: url({obj.background}); height: 50px; width: 100px; background-size: cover;"></div>')
+        return ""
+
+    font_preview.short_description = 'Font Preview'
+    logo_preview.short_description = 'Logo Preview'
+    background_preview.short_description = 'Background Preview'
+    font_preview.allow_tags = True
+    logo_preview.allow_tags = True
+    background_preview.allow_tags = True
