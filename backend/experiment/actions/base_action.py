dce11db8ad7461782bbd279b71181430799bbedb
Drikus Roor <drikusroor@gmail.com>
2024-02-21 14:22:20 +0100

    fix: Refactor BaseAction class to handle missing style parameter

--- backend/experiment/actions/base_action.py
+++ backend/experiment/actions/base_action.py
@@ -3,14 +3,17 @@ from .frontend_style import FrontendStyle
 
 class BaseAction(object):
     ID = 'BASE'
+    style = None
 
-    def __init__(self, style = FrontendStyle()):
+    def __init__(self, style: FrontendStyle = None):
         self.style = style
         pass
 
     def action(self):
         action_dict = self.__dict__
         action_dict['view'] = self.ID
-        action_dict['style'] = self.style.to_dict()
+
+        if self.style is not None:
+            action_dict['style'] = self.style.to_dict()
 
         return action_dict
