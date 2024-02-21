0ecc01872795d4428333ad1919051ac31bf41fd6
BeritJanssen <berit.janssen@gmail.com>
2024-02-21 08:57:42 +0100

    fix: implement redirects

--- frontend/src/components/ExperimentCollection/ExperimentCollection.js
+++ frontend/src/components/ExperimentCollection/ExperimentCollection.js
@@ -1,16 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 import { useExperimentCollection } from "../../API";
-import { useParticipantStore } from "../../util/stores";
 import Loading  from "../Loading/Loading";
 
-
 const ExperimentCollection = ({match}) => {
-    const urlQueryString = useRef(window.location.search); // location.search is a part of URL after (and incuding) "?"
-    const participant = useParticipantStore(state => state.participant);
-    const experiment = useRef(null);
     const [dashboard, setDashboard] = useState(undefined);
+    const [experiment, setExperiment] = useState(undefined);
     const [experimentCollection, loadingExperimentCollection] = useExperimentCollection(match.params.slug);
 
     useEffect(() => {
@@ -19,48 +15,34 @@ const ExperimentCollection = ({match}) => {
                 if (experimentCollection.dashboard) {
                     setDashboard(experimentCollection.dashboard);
                 } else {
-                    // redirect to experimentCollection.slug;
+                    setExperiment(experimentCollection);
                 }
             }
         }
     }, [experimentCollection, loadingExperimentCollection, setDashboard])
 
     return (
-        !loadingExperimentCollection && dashboard ? (
-        <div className="aha__collection">
-            {/* <Logo homeUrl={`/${experiment.slug}`} /> */}
-            {/* Experiments */}
-            <div className="dashboard">
-                <ul>
-                    {dashboard.map((experiment) => (
-                        <li
-                            key={experiment.slug}
-                            // style={{
-                            //     borderBottom: `5px solid ${experiment.color}`,
-                            // }}
-                        >
-                            <Link to={"/" + experiment.slug}>
-                                <div
-                                    className="image"
-                                    // style={{
-                                    //     backgroundImage: `url(${experiment.image})`,
-                                    //     backgroundColor: experiment.color,
-                                    // }}
-                                ></div>
-                                <h3>{experiment.name}</h3>
-                                {/* <p>{experiment.description}</p> */}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-            
-        ) :
-        (
+        loadingExperimentCollection? (
             <div className="loader-container">
                 <Loading />
             </div>
+        ) : dashboard? (
+            <div className="aha__collection">
+                {/* Experiments */}
+                <div className="dashboard">
+                    <ul>
+                        {dashboard.map((exp) => (
+                            <li key={exp.slug} >
+                                <Link to={"/" + exp.slug}>
+                                    <h3>{exp.name}</h3>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        ) : experiment && (
+            <Redirect to={"/" + experiment.slug} />
         )
     )
 }
