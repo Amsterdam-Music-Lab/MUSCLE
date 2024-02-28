0fc323e4b66dd83c6976b2f260409acaa972f0e8
Drikus Roor <drikusroor@gmail.com>
2024-02-28 12:46:53 +0100

    feat: Refactor FontLoader component to dynamically load and set font styles and add tests

--- frontend/src/components/FontLoader/FontLoader.js
+++ frontend/src/components/FontLoader/FontLoader.js
@@ -1,38 +1,42 @@
 import { useEffect } from 'react';
 
-const FontLoader = ({fontUrl, fontType = 'heading'}) => {
+const FontLoader = ({ fontUrl, fontType = 'heading' }) => {
     const fontMatch = /family=([^&:]+)/.exec(fontUrl);
     const font = fontMatch ? fontMatch[1].replace(/\+/g, ' ') : "sans-serif";
     const selector = fontType === 'heading' ? 'h1, h2, h3, h4, h5, h6, .btn' : 'body';
 
     useEffect(() => {
-
         if (!fontUrl) {
             return;
         }
 
-        // Dynamically load the font
-        const link = document.createElement('link');
-        link.href = fontUrl;
-        link.rel = 'stylesheet';
-        document.head.appendChild(link);
+        const linkId = `dynamic-font-link-${fontType}`
+        let link = document.getElementById(linkId);
+        if (!link) {
+            link = document.createElement('link');
+            link.id = linkId;
+            link.href = fontUrl;
+            link.rel = 'stylesheet';
+            document.head.appendChild(link);
+        }
 
-        // Set the global font style
-        const style = document.createElement('style');
-        style.innerHTML = `
-    ${selector} {
-        font-family: "${font}", sans-serif;
-    }
-`;
-        document.head.appendChild(style);
+        const styleId = `dynamic-font-style-${fontType}`;
+        let style = document.getElementById(styleId);
+        if (!style) {
+            style = document.createElement('style');
+            style.id = styleId;
+            style.innerHTML = `${selector} { font-family: "${font}", sans-serif; }`;
+            document.head.appendChild(style);
+        }
 
         return () => {
-            document.head.removeChild(link);
-            document.head.removeChild(style);
+            // Remove the link and style elements during cleanup
+            link && document.head.removeChild(link);
+            style && document.head.removeChild(style);
         };
-    }, [fontUrl]);
+    }, [fontUrl, font, selector]);
 
     return null;
-}
+};
 
 export default FontLoader;
