8322d24254820f182aee5bffc6ead1b1a9d73c07
Drikus Roor <drikusroor@gmail.com>
2024-03-05 11:07:16 +0100

    fix: Fix routing & spyOn issue in Experiment component tests

--- frontend/src/components/Experiment/Experiment.test.js
+++ frontend/src/components/Experiment/Experiment.test.js
@@ -1,47 +1,59 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Route, MemoryRouter } from 'react-router-dom';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import mockAxios from "jest-mock-axios";
-import { getNextRound } from '../../API';
+import * as API from '../../API';
 
 import Experiment from './Experiment';
 
-jest.mock("../../util/stores");
-
 const experimentObj = {
-    id: 24, slug: 'test', name: 'Test', playlists: [{id: 42, name: 'TestPlaylist'}],
-    next_round: [{view: 'INFO', button_label: 'Continue'}]
+    id: 24, slug: 'test', name: 'Test', playlists: [{ id: 42, name: 'TestPlaylist' }],
+    next_round: [{ view: 'INFO', button_label: 'Continue' }]
 };
-const nextRoundObj = {next_round: [{view: 'EXPLAINER', instruction: 'Instruction'}]};
 
+const nextRoundObj = { next_round: [{ view: 'EXPLAINER', instruction: 'Instruction' }] };
+
+jest.mock('../../util/stores', () => ({
+    __esModule: true,
+    default: (fn) => {
+        const state = {
+            session: 1,
+            participant: 'participant-id',
+        };
+        
+        return fn(state);
+    },
+    useBoundStore: jest.fn()
+}));  
 
 describe('Experiment Component', () => {
+
     afterEach(() => {
         mockAxios.reset();
     });
-    
 
     it('renders with given props', async () => {
-        mockAxios.get.mockResolvedValueOnce({data: experimentObj});
-        render(
-            <MemoryRouter>
-                <Experiment match={ {params: {slug: 'test'}} }/>
+        mockAxios.get.mockResolvedValueOnce({ data: experimentObj });
+        render((
+            <MemoryRouter initialEntries={['/test']}>
+                <Route path="/:slug" component={Experiment} />
             </MemoryRouter>
-        );
+        ));
         await screen.findByText('Continue');
     });
 
-    xit('calls onNext', async () => {
-        mockAxios.get.mockResolvedValueOnce({data: experimentObj});
+    it('calls onNext', async () => {
+        mockAxios.get.mockResolvedValueOnce({ data: experimentObj });
         render(
-            <MemoryRouter>
-                <Experiment match={ {params: {slug: 'test'}} }/>
+            <MemoryRouter initialEntries={['/test']}>
+                <Route path="/:slug" component={Experiment} />
             </MemoryRouter>
         );
         const button = await screen.findByText('Continue');
         fireEvent.click(button);
-        mockAxios.get.mockResolvedValueOnce({data: nextRoundObj});
-        await waitFor(() => expect(getNextRound).toHaveBeenCalled());
+        mockAxios.get.mockResolvedValueOnce({ data: nextRoundObj });
+        jest.spyOn(API, 'getNextRound');
+        await waitFor(() => expect(API.getNextRound).toHaveBeenCalled());
     });
 
 });
