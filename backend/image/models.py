3f7a23eaada60e913b02f47466dd61a72a42d339
Drikus Roor <drikusroor@gmail.com>
2024-03-06 19:31:25 +0100

    refactor: Make several Image fields optional

--- backend/image/models.py
+++ backend/image/models.py
@@ -5,11 +5,13 @@ from django.db import models
 class Image(models.Model):
     file = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    alt = models.CharField(max_length=255)
-    href = models.URLField()
-    rel = models.CharField(max_length=255)
-    target = models.CharField(max_length=255)
-    tags = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    description = models.TextField(blank=True, null=True)
+    alt = models.CharField(max_length=255, blank=True, null=True)
+    href = models.URLField(blank=True, null=True)
+    rel = models.CharField(max_length=255, blank=True, null=True)
+    target = models.CharField(max_length=255, blank=True, null=True)
+    tags = ArrayField(
+        models.CharField(max_length=255), blank=True, default=list, null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
