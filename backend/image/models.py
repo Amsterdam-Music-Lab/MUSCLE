e6bc187389eeb61023b7b3626465323427eda188
Drikus Roor <drikusroor@gmail.com>
2024-03-06 19:28:02 +0100

    feat: Add Image model to backend/image

--- backend/image/models.py
+++ backend/image/models.py
@@ -1,3 +1,15 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    file = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    alt = models.CharField(max_length=255)
+    href = models.URLField()
+    rel = models.CharField(max_length=255)
+    target = models.CharField(max_length=255)
+    tags = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
