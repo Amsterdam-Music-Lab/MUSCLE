e2faa9fb3ee0bbc5851a94669b9cce494f717774
Drikus Roor <drikusroor@gmail.com>
2024-03-14 11:02:44 +0100

    test: Test image, image placeholder and description props

--- frontend/src/components/ExperimentCollection/ExperimentCollection.test.js
+++ frontend/src/components/ExperimentCollection/ExperimentCollection.test.js
@@ -9,14 +9,15 @@ import ExperimentCollection from './ExperimentCollection';
 let mock = new MockAdapter(axios);
 
 describe('ExperimentCollection', () => {
-    const experiment1 = {
+    const experiment1 = getExperiment({
         slug: 'some_slug',
         name: 'Some Experiment'
-    };
-    const experiment2 = {
+    });
+    const experiment2 = getExperiment({
         slug: 'another_slug',
         name: 'Another Experiment'
-    };
+    });
+    const experimentWithAllProps = getExperiment({ image: 'some_image.jpg', description: 'Some description' });
 
     it('forwards to a single experiment if it receives a single object', () => {
         mock.onGet().replyOnce(200, experiment1);
@@ -41,5 +42,65 @@ describe('ExperimentCollection', () => {
         }) 
     });
 
-    
+    it('shows a loading spinner while loading', () => {
+        mock.onGet().replyOnce(200, new Promise(() => {}));
+        render(
+        <MemoryRouter>
+            <ExperimentCollection match={{params: {slug: 'some_collection'}}}/>
+        </MemoryRouter>
+        );
+        waitFor(() => {
+            expect(document.querySelector('.loader-container')).not.toBeNull();
+        })
+    });
+
+    it('shows a placeholder if no image is available', () => {
+        mock.onGet().replyOnce(200, new Promise(() => {
+            dashboard: [experiment1]
+        }));
+        render(
+        <MemoryRouter>
+            <ExperimentCollection match={{params: {slug: 'some_collection'}}}/>
+        </MemoryRouter>
+        );
+        waitFor(() => {
+            expect(document.querySelector('.loader-container')).not.toBeNull();
+        })
+    });
+
+    it('shows the image if it is available', () => {
+        mock.onGet().replyOnce(200, new Promise(() => {
+            dashboard: [experimentWithAllProps]
+        }));
+        render(
+        <MemoryRouter>
+            <ExperimentCollection match={{params: {slug: 'some_collection'}}}/>
+        </MemoryRouter>
+        );
+        waitFor(() => {
+            expect(document.querySelector('img')).not.toBeNull();
+        })
+    });
+
+    it('shows the description if it is available', () => {
+        mock.onGet().replyOnce(200, new Promise(() => {
+            dashboard: [experimentWithAllProps]
+        }));
+        render(
+        <MemoryRouter>
+            <ExperimentCollection match={{params: {slug: 'some_collection'}}}/>
+        </MemoryRouter>
+        );
+        waitFor(() => {
+            expect(screen.getByText('Some description')).toBeInTheDocument();
+        })
+    });
 })
+
+const getExperiment = (overrides = {}) => {
+    return {
+        slug: 'some_slug',
+        name: 'Some Experiment',
+        ...overrides
+    }
+}
