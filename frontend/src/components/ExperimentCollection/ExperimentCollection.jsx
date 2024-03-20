e47b630fef99260d711b1ce497a1b176aba528aa
Drikus Roor <drikusroor@gmail.com>
2024-03-20 18:07:17 +0100

    feat: Split Dashboard from Experiment Collection page and add subroutes to Dashboard & About pages

--- frontend/src/components/ExperimentCollection/ExperimentCollection.jsx
+++ frontend/src/components/ExperimentCollection/ExperimentCollection.jsx
@@ -1,38 +1,38 @@
-import { Link, Redirect } from "react-router-dom";
-
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect
+} from "react-router-dom";
 import { useExperimentCollection } from "../../API";
 import Loading from "../Loading/Loading";
-import { API_ROOT } from "../../config";
+import ExperimentCollectionAbout from "./ExperimentCollectionAbout/ExperimentCollectionAbout";
+import ExperimentCollectionDashboard from "./ExperimentCollectionDashboard/ExperimentCollectionDashboard";
+import { URLS } from "../../config";
 
 const ExperimentCollection = ({ match }) => {
-        /** ExperimentCollection is a Component which can show a dashboard of multiple experiments,
-     * or redirect to the next experiment in the collection,
-     * depending on the response from the backend, which has the following structure: 
-     * {
-     *   slug: string,
-     *   name: string,
-     *   description: string,
-     *   dashboard: [
-     *     {
-     *       slug: string,
-     *       name: string,
-     *       finished_session_count: number
-     *     }
-     *   ],
-     *   redirect_to: {
-     *     slug: string,
-     *     name: string,
-     *     finished_session_count: number
-     *   }
-     * }
-     * */ 
+    /** ExperimentCollection is a Component which can show a dashboard of multiple experiments,
+ * or redirect to the next experiment in the collection,
+ * depending on the response from the backend, which has the following structure: 
+ * {
+ *   slug: string,
+ *   name: string,
+ *   description: string,
+ *   dashboard: [
+ *     {
+ *       slug: string,
+ *       name: string,
+ *       finished_session_count: number
+ *     }
+ *   ],
+ *   redirect_to: {
+ *     slug: string,
+ *     name: string,
+ *     finished_session_count: number
+ *   }
+ * }
+ * */
     const [experimentCollection, loadingExperimentCollection] = useExperimentCollection(match.params.slug);
-    const dashboard = experimentCollection?.dashboard || [];
     const experimentToRedirectTo = experimentCollection?.redirect_to;
-
-    // TODO: get next experiment and about link from experimentCollection
-    const nextExperiment = null; // TODO: get next experiment from experimentCollection
-    const aboutLink = null; // TODO: get about link from experimentCollection
 
     if (loadingExperimentCollection) {
         return (
@@ -48,41 +48,12 @@ const ExperimentCollection = ({ match }) => {
 
     return (
         <div className="aha__collection">
-            <div class="hero">
-                <div class="intro">
-                    <p>{experimentCollection?.description}</p>
-                    <div class="actions">
-                        {nextExperiment && <a class="btn btn-lg btn-primary" href={"/" + nextExperiment.slug}>Volgende experiment</a>}
-                        {aboutLink && <a class="btn btn-lg btn-outline-primary" href="/toontjehoger/about">Over ons</a>}
-                    </div>
-                </div>
-                <div class="results">
-
-                </div>
-            </div>
-            {/* Experiments */}
-            <div role="menu" className="dashboard">
-                <ul>
-                    {dashboard.map((exp) => (
-                        <li key={exp.slug}>
-                            <Link to={"/" + exp.slug}>
-                                <ImageOrPlaceholder imagePath={exp.image} alt={exp.description} />
-                                <h3>{exp.name}</h3>
-                                <div role="status" className="counter">{exp.finished_session_count}</div>
-                            </Link>
-                        </li>
-                    ))}
-                    {dashboard.length === 0 && <p>No experiments found</p>}
-                </ul>
-            </div>
+            <Router>
+                <Route path={URLS.experimentCollectionAbout} component={() => <ExperimentCollectionAbout content={'## Hey!\n\n **lolz**'} />} />
+                <Route path={URLS.experimentCollection} exact component={() => <ExperimentCollectionDashboard experimentCollection={experimentCollection} />} />
+            </Router>
         </div>
     )
-}
-
-const ImageOrPlaceholder = ({ imagePath, alt }) => {
-    const imgSrc = imagePath ? `${API_ROOT}/${imagePath}` : null;
-
-    return imgSrc ? <img src={imgSrc} alt={alt} /> : <div className="placeholder" />;
 }
 
 export default ExperimentCollection;
