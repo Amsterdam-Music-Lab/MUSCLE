4a53b05b849ac4e782d77524161f8f0649c3c1ba
Drikus Roor <drikusroor@gmail.com>
2024-03-27 14:41:54 +0100

    refactor: Refactor PlayButton component to handle disabled state and allow multiple playbacks if argument (play_once = False) is present

--- frontend/src/components/PlayButton/PlayButton.jsx
+++ frontend/src/components/PlayButton/PlayButton.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 
-const PlayButton = ({ playSection, isPlaying, className="" }) => {
-
-    const [clicked, setClicked] = useState(false);
+const PlayButton = ({ playSection, isPlaying, className = "", disabled }) => {
 
     return (
         <>
-        <div
-            className={classNames("aha__play-button btn-blue border-outside", "btn", {
-                stop: isPlaying, disabled: clicked && !isPlaying,
-            },className)}
-            onClick={ (playSection && !clicked) ? () => {setClicked(true); playSection(0);} : undefined}
-            tabIndex="0"
-            onKeyPress={(e) => {
-                playSection && playSection(0)
-            }}
-        >
-        </div>
-        <div className="playbutton-spacer"></div>
+            <div
+                className={classNames("aha__play-button btn-blue border-outside", "btn", {
+                    stop: isPlaying, disabled: disabled || isPlaying
+                }, className)}
+                onClick={playSection && !disabled ? () => playSection(0) : undefined}
+                tabIndex="0"
+                onKeyPress={(e) => {
+                    playSection && playSection(0)
+                }}
+            >
+            </div>
+            <div className="playbutton-spacer"></div>
         </>
     );
 };
