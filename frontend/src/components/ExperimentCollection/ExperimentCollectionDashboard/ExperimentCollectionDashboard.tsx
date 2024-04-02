1337e7a9aaa304ece66331097582eefde7585eee
Drikus Roor <drikusroor@gmail.com>
2024-04-02 14:31:06 +0200

    Added: Experiment collection about page (#885)
    * chore: Rename all React component files to have a .jsx extension
    
    * chore(deps): Add @types/react dependency
    
    * config: Add tsconfigPaths plugin to Vite config
    
    * feat: Add Experiment Collection About page
    
    * feat: Split Dashboard from Experiment Collection page and add subroutes to Dashboard & About pages
    
    * feat: Display about content as html
    
    * chore: Rename tests to have .jsx extension
    
    * fix: Fix padding in experiment collection intro
    
    * style: Style about page content
    
    * chore(deps): Update @types/react to version 18.2.67
    
    * style: Add prose class for rendering markdown-related content
    
    * fix(test): Refactor ExperimentCollectionAbout component to include role attribute contentinfo
    
    * chore(deps): Update @types/react to version 18.2.72
    
    * chore(deps): Update @types/react-dom to version 18.2.22
    
    * refactor: Change extension ExperimentCollection to .tsx and start using typing
    
    * chore(deps): Update @types/react-dom to version 18.2.22 and remove @types/react-dom@npm:^18.2.22 from yarn.lock
    
    * chore(deps): Move react-router-dom types to dev deps
    
    * refactor: Refactor import statements to use the types folder instead of interfaces
    
    * chore(deps): Update @types/react and @types/react-dom to their latest versions
    
    * refactor: Refactor ExperimentCollectionAbout component to include slug prop and add "Terug" link
    
    * test: Add test for Terug link
    
    * chore: Rename test files to have tsx extension

---
+++ frontend/src/components/ExperimentCollection/ExperimentCollectionDashboard/ExperimentCollectionDashboard.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { API_ROOT } from "../../../config";
+import ExperimentCollection from "@/types/ExperimentCollection";
+
+
+interface ExperimentCollectionDashboardProps {
+    experimentCollection: ExperimentCollection;
+}
+
+export const ExperimentCollectionDashboard: React.FC<ExperimentCollectionDashboardProps> = ({ experimentCollection }) => {
+
+    const dashboard = experimentCollection?.dashboard || [];
+
+    // TODO: get next experiment and about link from experimentCollection
+    const nextExperiment = experimentCollection.next_experiment; // TODO: get next_experiment from experimentCollection
+    const aboutContent = experimentCollection.about_content;
+
+    return (
+        <>
+            <div className="hero">
+                <div className="intro">
+                    <p>{experimentCollection?.description}</p>
+                    <nav className="actions">
+                        {nextExperiment && <a className="btn btn-lg btn-primary" href={"/" + nextExperiment.slug}>Volgende experiment</a>}
+                        {aboutContent && <Link className="btn btn-lg btn-outline-primary" to={`/collection/${experimentCollection.slug}/about`}>Over ons</Link>}
+                    </nav>
+                </div>
+                <div className="results">
+
+                </div>
+            </div>
+            {/* Experiments */}
+            <div role="menu" className="dashboard">
+                <ul>
+                    {dashboard.map((exp) => (
+                        <li key={exp.slug}>
+                            <Link to={"/" + exp.slug}>
+                                <ImageOrPlaceholder imagePath={exp.image} alt={exp.description} />
+                                <h3>{exp.name}</h3>
+                                <div role="status" className="counter">{exp.finished_session_count}</div>
+                            </Link>
+                        </li>
+                    ))}
+                    {dashboard.length === 0 && <p>No experiments found</p>}
+                </ul>
+            </div>
+        </>
+    );
+}
+
+const ImageOrPlaceholder = ({ imagePath, alt }: { imagePath: string, alt: string }) => {
+    const imgSrc = imagePath ? `${API_ROOT}/${imagePath}` : null;
+
+    return imgSrc ? <img src={imgSrc} alt={alt} /> : <div className="placeholder" />;
+}
+
+
+export default ExperimentCollectionDashboard;
