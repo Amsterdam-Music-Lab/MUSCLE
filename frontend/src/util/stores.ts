e8827de777bf858e87b61cac2a16a9ab4b84e088
Drikus Roor <drikusroor@gmail.com>
2024-04-08 13:15:10 -0300

    refactor: Migrate Zustand store to Typescript and add optional Sentry error capture

---
+++ frontend/src/util/stores.ts
@@ -0,0 +1,57 @@
+import { create, StateCreator } from "zustand";
+import * as Sentry from '@sentry/react';
+import { Participant } from "@/types/Participant";
+
+interface ErrorSlice {
+    error: string | null;
+    setError: (message: string, errorToCapture?: Error) => void;
+}
+
+const createErrorSlice: StateCreator<ErrorSlice> = (set) => ({
+    error: null,
+    setError: (message, errorToCapture) => {
+        set(() => ({ error: message }));
+        if (errorToCapture) {
+            Sentry.captureException(errorToCapture);
+        }
+    }
+});
+
+interface ParticipantSlice {
+    participant: Participant | null;
+    setParticipant: (participant: Participant) => void;
+}
+
+const createParticipantSlice: StateCreator<ParticipantSlice> = (set) => ({
+    participant: null,
+    setParticipant: (participant) => set(() => ({ participant }))
+});
+
+interface SessionSlice {
+    session: string | null;
+    setSession: (session: string) => void;
+}
+
+const createSessionSlice: StateCreator<SessionSlice> = (set) => ({
+    session: null,
+    setSession: (session) => set(() => ({ session }))
+});
+
+interface ThemeSlice {
+    theme: string | null;
+    setTheme: (theme: string) => void;
+}
+
+const createThemeSlice: StateCreator<ThemeSlice> = (set) => ({
+    theme: null,
+    setTheme: (theme) => set(() => ({ theme })),
+});
+
+export const useBoundStore = create<ErrorSlice & ParticipantSlice & SessionSlice & ThemeSlice>((...args) => ({
+    ...createErrorSlice(...args),
+    ...createParticipantSlice(...args),
+    ...createSessionSlice(...args),
+    ...createThemeSlice(...args),
+}));
+
+export default useBoundStore;
