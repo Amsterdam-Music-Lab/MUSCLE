7f613f2f892aade580e0a0ac602dd687dc7ab0c3
Drikus Roor <drikusroor@gmail.com>
2024-04-09 11:08:08 -0300

    chore(deps): Upgrade storybook from 8.0.4 to 8.0.6 and use Storybook's migration tool to fix several other issues (#924)

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -10,7 +10,6 @@ initWebAudioListener();
 /** @type { import('@storybook/react').Preview } */
 const preview = {
     parameters: {
-        actions: { argTypesRegex: "^on[A-Z].*" },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
