15bdf8a4d8918c74afd80a6229160182198b2664
Drikus Roor <drikusroor@gmail.com>
2024-04-14 17:00:45 -0300

    test: Add test for social media info in BaseTest

---
+++ backend/experiment/rules/tests/test_base.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from django.conf import settings
+from experiment.models import Experiment
+from ..base import Base
+
+
+class BaseTest(TestCase):
+
+    def test_social_media_info(self):
+        reload_participant_target = settings.RELOAD_PARTICIPANT_TARGET
+        slug = 'music-lab'
+        experiment = Experiment.objects.create(
+            name='Music Lab',
+            slug=slug,
+        )
+        base = Base()
+        social_media_info = base.social_media_info(
+            experiment=experiment,
+            score=100,
+        )
+
+        expected_url = f"{reload_participant_target}/{slug}"
+
+        self.assertEqual(social_media_info['apps'], ['facebook', 'twitter'])
+        self.assertEqual(social_media_info['message'], 'I scored 100 points on https://app.amsterdammusiclab.nl/music-lab')
+        self.assertEqual(social_media_info['url'], expected_url)
+        self.assertNotContains(social_media_info['url'], '//') # Check for double slashes
+        self.assertEqual(social_media_info['hashtags'], ['music-lab', 'amsterdammusiclab', 'citizenscience'])
+
+        from django.conf import settings
+        # Override settings with production values
+        # import production settings
+        import aml.production_settings
+        settings.configure(aml.production_settings)
+
+
+        experiment = Experiment.objects.create(
+            name='Music Lab',
+            slug='music-lab',
+        )
+        base = Base()
+        social_media_info = base.social_media_info(
+            experiment=experiment,
+            score=100,
+        )
+
+        self.assertEqual(social_media_info['apps'], ['facebook', 'twitter'])
+        self.assertEqual(social_media_info['message'], 'I scored 100 points on https://app.amsterdammusiclab.nl/music-lab')
+        self.assertEqual(social_media_info['url'], 'https://app.amsterdammusiclab.nl/music-lab')
+        self.assertEqual(social_media_info['hashtags'], ['music-lab', 'amsterdammusiclab', 'citizenscience'])
