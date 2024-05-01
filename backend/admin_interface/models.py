87ea21db8c102dc62be993b5cc67db736ef63ebe
Drikus Roor <drikusroor@gmail.com>
2024-05-01 16:16:38 +0200

    fix: Update color values in AdminInterfaceThemeConfiguration model to work with color inputs
    The commit modifies the `AdminInterfaceThemeConfiguration` model in the `models.py` file. It updates the default color values for various elements such as primary, secondary, accent colors, body colors, header color, link colors, borders, error colors, message colors, darkened colors, selected colors, and button colors.
    
    The purpose of this change is to ensure consistent and visually appealing theming for the admin interface.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative mood, followed by a brief description of the changes.

--- backend/admin_interface/models.py
+++ backend/admin_interface/models.py
@@ -30,35 +30,35 @@ class AdminInterfaceThemeConfiguration(models.Model):
     color_primary = models.CharField(max_length=255, blank=True, default='#79aec8')
     color_secondary = models.CharField(max_length=255, blank=True, default='#417690')
     color_accent = models.CharField(max_length=255, blank=True, default='#f5dd5d')
-    color_primary_fg = models.CharField(max_length=255, blank=True, default='#fff')
+    color_primary_fg = models.CharField(max_length=255, blank=True, default='#ffffff')
 
     ### Body
-    color_body_fg = models.CharField(max_length=255, blank=True, default='#333')
-    color_body_bg = models.CharField(max_length=255, blank=True, default='#fff')
-    color_body_quiet_color = models.CharField(max_length=255, blank=True, default='#666')
-    color_body_loud_color = models.CharField(max_length=255, blank=True, default='#000')
+    color_body_fg = models.CharField(max_length=255, blank=True, default='#333333')
+    color_body_bg = models.CharField(max_length=255, blank=True, default='#ffffff')
+    color_body_quiet_color = models.CharField(max_length=255, blank=True, default='#666666')
+    color_body_loud_color = models.CharField(max_length=255, blank=True, default='#000000')
 
     ### Header
-    color_header_color = models.CharField(max_length=255, blank=True, default='#ffc')
+    color_header_color = models.CharField(max_length=255, blank=True, default='#ffffcc')
 
     ### Breadcumbs
     color_breadcrumbs_fg = models.CharField(max_length=255, blank=True, default='#c4dce8')
 
     ### Link
     color_link_fg = models.CharField(max_length=255, blank=True, default='#447e9b')
-    color_link_hover_color = models.CharField(max_length=255, blank=True, default='#036')
+    color_link_hover_color = models.CharField(max_length=255, blank=True, default='#003366')
     color_link_selected_fg = models.CharField(max_length=255, blank=True, default='#5b80b2')
 
     ### Borders
     color_hairline_color = models.CharField(max_length=255, blank=True, default='#e8e8e8')
-    color_border_color = models.CharField(max_length=255, blank=True, default='#ccc')
+    color_border_color = models.CharField(max_length=255, blank=True, default='#cccccc')
 
     ### Error
     color_error_fg = models.CharField(max_length=255, blank=True, default='#ba2121')
 
     ### Message
-    color_message_success_bg = models.CharField(max_length=255, blank=True, default='#dfd')
-    color_message_warning_bg = models.CharField(max_length=255, blank=True, default='#ffc')
+    color_message_success_bg = models.CharField(max_length=255, blank=True, default='#ddffdd')
+    color_message_warning_bg = models.CharField(max_length=255, blank=True, default='#ffffcc')
     color_message_error_bg = models.CharField(max_length=255, blank=True, default='#ffefef')
 
     ### Darkened
@@ -66,15 +66,15 @@ class AdminInterfaceThemeConfiguration(models.Model):
 
     ### Selected
     color_selected_bg = models.CharField(max_length=255, blank=True, default='#e4e4e4')
-    color_selected_row = models.CharField(max_length=255, blank=True, default='#ffc')
+    color_selected_row = models.CharField(max_length=255, blank=True, default='#ffffcc')
 
     ### Button
-    color_button_fg = models.CharField(max_length=255, blank=True, default='#fff')
+    color_button_fg = models.CharField(max_length=255, blank=True, default='#ffffff')
     color_button_bg = models.CharField(max_length=255, blank=True, default='#79aec8')
     color_button_hover_bg = models.CharField(max_length=255, blank=True, default='#609ab6')
     color_default_button_bg = models.CharField(max_length=255, blank=True, default='#417690')
     color_default_button_hover_bg = models.CharField(max_length=255, blank=True, default='#205067')
-    color_close_button_bg = models.CharField(max_length=255, blank=True, default='#888')
+    color_close_button_bg = models.CharField(max_length=255, blank=True, default='#888888')
     color_close_button_hover_bg = models.CharField(max_length=255, blank=True, default='#747474')
     color_delete_button_bg = models.CharField(max_length=255, blank=True, default='#ba2121')
     color_delete_button_hover_bg = models.CharField(max_length=255, blank=True, default='#a41515')
