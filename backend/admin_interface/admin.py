1153f72437857b07e1b2f09361e65769b3e88c84
Drikus Roor <drikusroor@gmail.com>
2024-05-01 14:32:36 +0200

    feat: Add admin interface URLs and views
    This commit adds the necessary URLs and views for the admin interface app. It includes a new `admin_interface/urls.py` file with a route for the `get_theme_stylesheet` view. Additionally, the `backend/aml/urls.py` file is modified to include the `admin_interface` URLs.
    
    The purpose of this change is to enable theming and styling for the global admin dashboard.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative mood, followed by a brief description of the changes.

--- backend/admin_interface/admin.py
+++ backend/admin_interface/admin.py
@@ -5,7 +5,73 @@ from .models import AdminInterfaceConfiguration, AdminInterfaceThemeConfiguratio
 class AdminInterfaceThemeConfigurationInline(admin.StackedInline):
     model = AdminInterfaceThemeConfiguration
     extra = 0
-    fields = ('color_default_bg', 'color_default_fg', 'color_success_bg', 'color_success_fg', 'color_warning_bg', 'color_warning_fg', 'color_error_bg', 'color_error_fg')
+    fields = (
+        # Color scheme
+        
+        ## Official Django colors
+
+        ### Main colors
+        'color_primary',
+        'color_secondary',
+        'color_accent',
+        'color_primary_fg',
+
+        ### Body
+        'color_body_fg',
+        'color_body_bg',
+        'color_body_quiet_color',
+        'color_body_loud_color',
+
+        ### Header
+        'color_header_color',
+
+        ### Breadcumbs
+        'color_breadcrumbs_fg',
+
+        ### Link
+        'color_link_fg',
+        'color_link_hover_color',
+        'color_link_selected_fg',
+
+        ### Borders
+        'color_hairline_color',
+        'color_border_color',
+
+        ### Error
+        'color_error_fg',
+
+        ### Message
+        'color_message_success_bg',
+        'color_message_warning_bg',
+        'color_message_error_bg',
+
+        ### Darkened
+        'color_darkened_bg',
+
+        ### Selected
+        'color_selected_bg',
+        'color_selected_row',
+
+        ### Button
+        'color_button_fg',
+        'color_button_bg',
+        'color_button_hover_bg',
+        'color_default_button_bg',
+        'color_default_button_hover_bg',
+        'color_close_button_bg',
+        'color_close_button_hover_bg',
+        'color_delete_button_bg',
+        'color_delete_button_hover_bg',
+
+        # Custom colors
+        'color_default_bg',
+        'color_default_fg',
+        'color_success_bg',
+        'color_success_fg',
+        'color_warning_bg',
+        'color_warning_fg',
+        'color_error_bg',
+    )
 
 
 class AdminInterfaceConfigurationAdmin(admin.ModelAdmin):
@@ -14,5 +80,7 @@ class AdminInterfaceConfigurationAdmin(admin.ModelAdmin):
     inlines = [AdminInterfaceThemeConfigurationInline]
 
 
-admin.site.register(AdminInterfaceConfiguration, AdminInterfaceConfigurationAdmin)
-admin.site.register(AdminInterfaceThemeConfiguration)
+admin.site.register(
+    AdminInterfaceConfiguration,
+    AdminInterfaceConfigurationAdmin,
+)
--- backend/admin_interface/models.py
+++ backend/admin_interface/models.py
@@ -5,6 +5,7 @@ class AdminInterfaceConfiguration(models.Model):
     """Model for storing the configuration of the admin interface"""
     name = models.CharField(max_length=255, unique=True, default='Default')
     description = models.TextField(blank=True, default='')
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -22,6 +23,63 @@ class AdminInterfaceThemeConfiguration(models.Model):
         AdminInterfaceConfiguration, on_delete=models.CASCADE, related_name='theme')
 
     # Color scheme
+
+    ## Official Django colors
+
+    ### Main colors
+    color_primary = models.CharField(max_length=255, blank=True, default='#79aec8')
+    color_secondary = models.CharField(max_length=255, blank=True, default='#417690')
+    color_accent = models.CharField(max_length=255, blank=True, default='#f5dd5d')
+    color_primary_fg = models.CharField(max_length=255, blank=True, default='#fff')
+
+    ### Body
+    color_body_fg = models.CharField(max_length=255, blank=True, default='#333')
+    color_body_bg = models.CharField(max_length=255, blank=True, default='#fff')
+    color_body_quiet_color = models.CharField(max_length=255, blank=True, default='#666')
+    color_body_loud_color = models.CharField(max_length=255, blank=True, default='#000')
+
+    ### Header
+    color_header_color = models.CharField(max_length=255, blank=True, default='#ffc')
+
+    ### Breadcumbs
+    color_breadcrumbs_fg = models.CharField(max_length=255, blank=True, default='#c4dce8')
+
+    ### Link
+    color_link_fg = models.CharField(max_length=255, blank=True, default='#447e9b')
+    color_link_hover_color = models.CharField(max_length=255, blank=True, default='#036')
+    color_link_selected_fg = models.CharField(max_length=255, blank=True, default='#5b80b2')
+
+    ### Borders
+    color_hairline_color = models.CharField(max_length=255, blank=True, default='#e8e8e8')
+    color_border_color = models.CharField(max_length=255, blank=True, default='#ccc')
+
+    ### Error
+    color_error_fg = models.CharField(max_length=255, blank=True, default='#ba2121')
+
+    ### Message
+    color_message_success_bg = models.CharField(max_length=255, blank=True, default='#dfd')
+    color_message_warning_bg = models.CharField(max_length=255, blank=True, default='#ffc')
+    color_message_error_bg = models.CharField(max_length=255, blank=True, default='#ffefef')
+
+    ### Darkened
+    color_darkened_bg = models.CharField(max_length=255, blank=True, default='#f8f8f8')
+
+    ### Selected
+    color_selected_bg = models.CharField(max_length=255, blank=True, default='#e4e4e4')
+    color_selected_row = models.CharField(max_length=255, blank=True, default='#ffc')
+
+    ### Button
+    color_button_fg = models.CharField(max_length=255, blank=True, default='#fff')
+    color_button_bg = models.CharField(max_length=255, blank=True, default='#79aec8')
+    color_button_hover_bg = models.CharField(max_length=255, blank=True, default='#609ab6')
+    color_default_button_bg = models.CharField(max_length=255, blank=True, default='#417690')
+    color_default_button_hover_bg = models.CharField(max_length=255, blank=True, default='#205067')
+    color_close_button_bg = models.CharField(max_length=255, blank=True, default='#888')
+    color_close_button_hover_bg = models.CharField(max_length=255, blank=True, default='#747474')
+    color_delete_button_bg = models.CharField(max_length=255, blank=True, default='#ba2121')
+    color_delete_button_hover_bg = models.CharField(max_length=255, blank=True, default='#a41515')
+
+    ## Custom colors
     color_default_bg = models.CharField(max_length=255, blank=True, default='#f8d7da')
     color_default_fg = models.CharField(max_length=255, blank=True, default='#721c24')
     color_success_bg = models.CharField(max_length=255, blank=True, default='#d4edda')
@@ -29,4 +87,3 @@ class AdminInterfaceThemeConfiguration(models.Model):
     color_warning_bg = models.CharField(max_length=255, blank=True, default='#fff3cd')
     color_warning_fg = models.CharField(max_length=255, blank=True, default='#856404')
     color_error_bg = models.CharField(max_length=255, blank=True, default='#f8d7da')
-    color_error_fg = models.CharField(max_length=255, blank=True, default='#721c24')
