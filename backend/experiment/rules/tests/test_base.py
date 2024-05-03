43c198e7f3814a38fb19311da6e61d23ab47e234
Drikus Roor <drikusroor@gmail.com>
2024-05-03 15:07:07 +0200

    test: Add playlist validation test cases

--- backend/experiment/rules/tests/test_base.py
+++ backend/experiment/rules/tests/test_base.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.conf import settings
 from experiment.models import Experiment
+from section.models import Playlist
 from ..base import Base
 
 
@@ -27,3 +28,13 @@ class BaseTest(TestCase):
         # Check for double slashes
         self.assertNotIn(social_media_info['url'], '//')
         self.assertEqual(social_media_info['hashtags'], ['music-lab', 'amsterdammusiclab', 'citizenscience'])
+
+    def test_validate_playlist(self):
+        base = Base()
+        playlist = None
+        errors = base.validate_playlist(playlist)
+        self.assertEqual(errors, ['The experiment must have a playlist.'])
+
+        playlist = Playlist()
+        errors = base.validate_playlist(playlist)
+        self.assertEqual(errors, ['The experiment must have at least one section.'])
