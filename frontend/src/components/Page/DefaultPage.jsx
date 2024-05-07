4c4e32b62ee633338afba9868de0b5df5c747b3d
BeritJanssen <berit.janssen@gmail.com>
2024-05-07 15:25:29 +0200

    roll back changes to DefaultPage

--- frontend/src/components/Page/DefaultPage.jsx
+++ frontend/src/components/Page/DefaultPage.jsx
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from "react";
-import useBoundStore from "../../util/stores";
-
+import React from "react";
 import Page from "./Page";
 import AppBar from "../AppBar/AppBar";
-import Header from "../Header/Header";
 
 // DefaultPage is a Page with an AppBar and a width-restricted container for content
-const DefaultPage = ({ className, title, logoClickConfirm, collectionSlug='', nextExperimentSlug='', children }) => {
-
-    const theme = useBoundStore((state) => state.theme);
-    const [headerProps, setHeaderProps] = useState(undefined);
-
-    useEffect(() => {
-        if (theme && theme.header) {
-            const headerProps = {
-                collectionSlug,
-                nextExperimentSlug,
-                aboutButtonText: theme.header.about_button_text,
-                nextExperimentButtonText: theme.header.next_experiment_button_text,
-                showScore: theme.header.show_score
-            };
-            setHeaderProps(headerProps);
-        }
-    }, [theme])
-    
-
+const DefaultPage = ({ className, title, logoClickConfirm, children }) => {
     return (
         <Page className={className}>
             <AppBar title={title} logoClickConfirm={logoClickConfirm} />
-            {headerProps && (
-                <Header {...headerProps }></Header>
-            )}
             <div className="container">
                 <div className="row justify-content-center py-3">
                     <div className="col-12">{children}</div>
