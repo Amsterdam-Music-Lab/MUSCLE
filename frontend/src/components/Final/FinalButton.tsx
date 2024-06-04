6c1f9d566cdc3ee5a35f54676eaefc3c454aab97
Drikus Roor <drikusroor@gmail.com>
2024-06-04 12:48:44 +0200

    docs: Add comments to FinalButton component

--- frontend/src/components/Final/FinalButton.tsx
+++ frontend/src/components/Final/FinalButton.tsx
@@ -19,6 +19,7 @@ const FinalButton: React.FC<FinalButtonProps> = ({ button, onNext }) => {
         return null;
     }
 
+    // If the button does not have a link, it will call the onNext function using a button click event
     if (!button.link) {
         return (
             <button data-testid="button" className='btn btn-primary btn-lg' onClick={() => onNext(false)}>
@@ -27,6 +28,7 @@ const FinalButton: React.FC<FinalButtonProps> = ({ button, onNext }) => {
         )
     }
 
+    // If the button has a link, it will render a Link component if the link is a relative URL
     if (isRelativeUrl(button.link)) {
         return (
             <Link data-testid="button-link" className='btn btn-primary btn-lg' to={button.link}>
@@ -35,6 +37,7 @@ const FinalButton: React.FC<FinalButtonProps> = ({ button, onNext }) => {
         )
     }
 
+    // If the button has a link, it will render an anchor tag if the link is an absolute URL
     return (
         <a data-testid="button-link" className='btn btn-primary btn-lg' href={button.link} target="_blank" rel="noopener noreferrer">
             {button.text}
