cc9f4a2cf6396174409b056d3e51bc4bec7fb3ce
Evert-R <evert.rot@gmx.com>
2024-06-04 07:25:17 +0200

    code correction

--- frontend/src/components/ExperimentCollection/ExperimentCollectionDashboard/ExperimentCollectionDashboard.tsx
+++ frontend/src/components/ExperimentCollection/ExperimentCollectionDashboard/ExperimentCollectionDashboard.tsx
@@ -20,7 +20,7 @@ export const ExperimentCollectionDashboard: React.FC<ExperimentCollectionDashboa
     const headerProps = experimentCollection.theme?.header? {
         nextExperimentSlug,        
         collectionSlug: experimentCollection.slug,
-        ...experimentCollection.theme.header,
+        ... experimentCollection.theme.header,
         totalScore: totalScore
         
     } : undefined;
