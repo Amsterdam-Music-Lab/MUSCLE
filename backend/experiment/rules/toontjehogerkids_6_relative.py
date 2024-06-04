140f1e0c8c4cf508572c9ec67bc47e34c2205909
BeritJanssen <berit.janssen@gmail.com>
2024-06-04 08:01:49 +0200

    fix first_round and implement inheritance toontjehoger 6

--- backend/experiment/rules/toontjehogerkids_6_relative.py
+++ backend/experiment/rules/toontjehogerkids_6_relative.py
@@ -2,22 +2,19 @@ import logging
 from django.template.loader import render_to_string
 from os.path import join
 from .toontjehoger_1_mozart import toontjehoger_ranks
+from .toontjehoger_6_relative import ToontjeHoger6Relative
 from experiment.actions import Trial, Explainer, Step, Score, Final, Playlist, Info
 from experiment.actions.form import ChoiceQuestion, Form
 from experiment.actions.playback import Multiplayer
 from experiment.actions.styles import STYLE_BOOLEAN
-from .base import Base
 
 from result.utils import prepare_result
 
 logger = logging.getLogger(__name__)
 
 
-class ToontjeHogerKids6Relative(Base):
+class ToontjeHogerKids6Relative(ToontjeHoger6Relative):
     ID = 'TOONTJE_HOGER_KIDS_6_RELATIVE'
-    TITLE = ""
-    SCORE_CORRECT = 50
-    SCORE_WRONG = 0
 
     def first_round(self, experiment):
         """Create data for the first experiment rounds."""
@@ -37,30 +34,9 @@ class ToontjeHogerKids6Relative(Base):
             button_label="Start"
         )
 
-        # 2. Choose playlist.
-        playlist = Playlist(experiment.playlists.all())
-
         return [
             explainer,
-            playlist,
         ]
-
-    def next_round(self, session):
-        """Get action data for the next round"""
-
-        rounds_passed = session.rounds_passed()
-
-        # Round 1
-        if rounds_passed == 0:
-            # No combine_actions because of inconsistent next_round array wrapping in first round
-            return self.get_round(rounds_passed, session)
-
-        # Round 2
-        if rounds_passed == 1:
-            return [*self.get_score(session), *self.get_round(round, session)]
-
-        # Final
-        return self.get_final_round(session)
 
     def get_score(self, session):
         # Feedback
@@ -79,65 +55,6 @@ class ToontjeHogerKids6Relative(Base):
         config = {'show_total_score': True}
         score = Score(session, config=config, feedback=feedback)
         return [score]
-
-    def get_round(self, round, session):
-
-        # Config
-        # -----------------
-        # section 1 is always section 'a'
-        section1 = session.section_from_any_song(
-            filter_by={'tag': 'a'})
-        if section1 is None:
-            raise Exception(
-                "Error: could not find section1 for round {}".format(round))
-
-        # Get correct tag for round 0 or 1
-        tag = 'b' if round == 0 else 'c'
-        section2 = session.section_from_any_song(
-            filter_by={'tag': tag})
-        if section2 is None:
-            raise Exception(
-                "Error: could not find section2 for round {}".format(round))
-
-        # Fragments A,B,C are all different, so answer is always NO
-        expected_response = "NO"
-
-        # Question
-        key = 'same_melody'
-        question = ChoiceQuestion(
-            question="Zijn deze twee melodieën hetzelfde?",
-            key=key,
-            choices={
-                "YES": "Ja",
-                "NO": "Nee",
-            },
-            view='BUTTON_ARRAY',
-            submits=True,
-            style=STYLE_BOOLEAN,
-            result_id=prepare_result(
-                key, session, section=section1,
-                expected_response=expected_response
-            )
-        )
-        form = Form([question])
-
-        # Player
-        playback = Multiplayer(
-            [section1, section2],
-            play_once=True,
-            labels=['A', 'B' if round == 0 else 'C']
-        )
-
-        trial = Trial(
-            playback=playback,
-            feedback_form=form,
-            title=self.TITLE,
-            style='blue-players'
-        )
-        return [trial]
-
-    def calculate_score(self, result, data):
-        return self.SCORE_CORRECT if result.expected_response == result.given_response else self.SCORE_WRONG
 
     def get_final_round(self, session):
 
