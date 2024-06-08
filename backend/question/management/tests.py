89b0b33377762aed5d07594dc012a9e48cbba198
albertas-jn <24507839+albertas-jn@users.noreply.github.com>
2024-06-08 12:32:37 +0200

    Remove call_command() from test_createquestions()

--- backend/question/management/tests.py
+++ backend/question/management/tests.py
@@ -6,7 +6,6 @@ class CreateQuestionsTest(TestCase):
 
     def test_createquestions(self):
         from question.models import Question, QuestionGroup
-        call_command('bootstrap')
         self.assertEqual(len(Question.objects.all()), 161) # Only built-in questions in test database
         self.assertEqual(len(QuestionGroup.objects.all()), 18) # Only built-in question groups in test database
         self.assertEqual(len(Question.objects.filter(key='dgf_country_of_origin')), 1)
