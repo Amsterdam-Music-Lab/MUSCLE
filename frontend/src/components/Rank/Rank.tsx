613e9a6baecbd14e7f5e25524bbb6b1560ae32b7
Drikus Roor <drikusroor@gmail.com>
2024-06-19 12:14:58 +0200

    refactor: Convert Rank component to use TypeScript

--- frontend/src/components/Rank/Rank.tsx
+++ frontend/src/components/Rank/Rank.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import classNames from "classnames";
 
+interface RankProps {
+    rank: {
+        class: string;
+        text: string;
+    };
+}
+
 // Rank shows a decorated representation of a rank
-const Rank = ({ rank }) => {
+const Rank = ({ rank }: RankProps) => {
     return (
         <div
             className={classNames("aha__rank", rank.class, {
