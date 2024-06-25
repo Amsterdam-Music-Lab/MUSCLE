d6e9ca2a04523f09dcc4df9d68eebfc4b60b2031
Drikus Roor <drikusroor@gmail.com>
2024-06-25 13:43:33 +0200

    refactor: Update Score component to use rank class for styling

--- frontend/src/components/ExperimentCollection/Score/Score.tsx
+++ frontend/src/components/ExperimentCollection/Score/Score.tsx
@@ -6,20 +6,25 @@ import useAnimatedScore from "@/hooks/useAnimatedScore";
 interface ScoreProps {
     score: number;
     label: string;
-    scoreClass: string;
+    scoreClass: string; // The rank class, e.g. diamond, platinum, gold, silver, bronze, plastic
 }
 
 const Score = ({ score, label, scoreClass }: ScoreProps) => {
     const currentScore = useAnimatedScore(score);
 
+    const rank = {
+        class: scoreClass,
+        text: "",
+    }
+
     return (
         <div className="score">
-            <Rank rank={{ class: scoreClass }} />
+            <Rank rank={rank} />
             <h3>
                 {currentScore ? currentScore + " " : ""}
                 {label}
             </h3>
-        </div>
+        </div >
     );
 };
 
