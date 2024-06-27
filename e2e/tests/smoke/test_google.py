7f3df163b45b81fa062aa8fd4b0bb8a71b702a2b
Drikus Roor <drikusroor@gmail.com>
2024-06-27 15:02:17 +0200

    refactor: Also use cloudflare to test connection

--- e2e/tests/smoke/test_google.py
+++
@@ -1,8 +0,0 @@
-from tests.base_test import BaseTest
-
-
-class TestGoogle(BaseTest):
-    def test_google(self):
-        self.driver.get("http://www.google.com")
-        self.assertIn("Google", self.driver.title)
-        print("\n✅ Google (and thus the internet) is working!")
