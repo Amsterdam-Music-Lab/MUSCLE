f20ca9ac336abfe9caa57ed83967d5dc071d3287
Drikus Roor <drikusroor@gmail.com>
2024-07-05 14:08:57 +0200

    refactor: Convert TextRange to TypeScript

--- frontend/src/components/Question/_TextRange.tsx
+++ frontend/src/components/Question/_TextRange.tsx
@@ -1,22 +1,32 @@
-import React from "react";
 import Slider from "react-rangeslider";
 import classNames from "classnames";
 
 import RangeLimits from "./_RangeLimits";
 import RangeTitle from "./_RangeTitle";
+import Question from "@/types/Question";
+
+interface TextRangeProps {
+    question: Question;
+    value: string;
+    onChange: (value: string) => void;
+}
 
 /**
  * TextRange is a question view that makes you select a value within the given range, using a slider from a list of choices
  * Values are multiplied by 10 to be displayed as a slider.
  * This to ensure that the slider is centered initially, even if we don't have a center value
  *  */
-const TextRange = ({ question, value, onChange, emphasizeTitle }) => {
+const TextRange = ({ question, value, onChange }: TextRangeProps) => {
     const emptyValue = !value;
+
+    if (!question.choices || Object.keys(question.choices).length === 0) {
+        throw new Error("TextRange question must have choices");
+    }
 
     const keys = Object.keys(question.choices);
     const choices = Object.values(question.choices);
 
-    const onSliderChange = (index) => onChange(keys[Math.round(index / 10)]);
+    const onSliderChange = (index: number) => onChange(keys[Math.round(index / 10)]);
 
     let sliderValue = 0;
     if (emptyValue) {
@@ -29,7 +39,6 @@ const TextRange = ({ question, value, onChange, emphasizeTitle }) => {
         <div className={classNames("aha__text-range", { empty: emptyValue })}>
 
             <RangeTitle
-                emphasizeTitle={emphasizeTitle}
                 question={question}
                 value={value}
                 sliderValue={sliderValue}
