fc7b894c8d896cd1f17654a5ee0e949bacca8cf3
Drikus Roor <drikusroor@gmail.com>
2024-07-08 14:52:22 +0200

    feat: Show language tabs for translated fields

--- backend/theme/admin.py
+++ backend/theme/admin.py
@@ -50,6 +50,16 @@ class ThemeConfigAdmin(TranslationAdmin):
         }),
     )
 
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
+
     def header_overview(self, obj):
         return 'Header set' if obj.header else ''
 
