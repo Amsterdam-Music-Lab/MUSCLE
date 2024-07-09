92a1167f7cc030ac9fd7e93c2a02dbb0bd888c52
Drikus Roor <drikusroor@gmail.com>
2024-07-09 11:27:56 +0200

    refactor: Rename showExperimentSessions to showBlockSessions for consistency

--- backend/experiment/static/collection_dashboard_sessions.js
+++ backend/experiment/static/collection_dashboard_sessions.js
@@ -1,6 +1,6 @@
 // Show sessions for a specific experiment
-const showExperimentSessions = (id) => {
-    document.getElementById(`show-sessions-${id}`).addEventListener("click", function() {                            
+const showBlockSessions = (id) => {
+    document.getElementById(`show-sessions-${id}`).addEventListener("click", function () {
         document.querySelectorAll(`.experiment-${id}`).forEach(function (element) {
             element.classList.remove("hide");
         })
@@ -10,7 +10,7 @@ const showExperimentSessions = (id) => {
 
 // Show sessions for a specific participant
 const showParticipantSessions = (id) => {
-    document.getElementById(`participant-sessions-${id}`).addEventListener("click", function() {                            
+    document.getElementById(`participant-sessions-${id}`).addEventListener("click", function () {
         document.querySelectorAll(`.participant-session-${id}`).forEach(function (element) {
             element.classList.remove("hide");
         })
