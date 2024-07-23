ceed4bd10d79c7f9c5e0909c549d1849620c619c
Drikus Roor <drikusroor@gmail.com>
2024-07-23 10:35:06 +0200

    chore: Format some files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@/': '/src/',
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        strictPort: true,
     },
-  },
+    build: {
+        outDir: 'build',
+        sourcemap: true,
+    },
+    resolve: {
+        alias: {
+            '@/': '/src/',
+        },
+    },
 })
