5ee1be4ad093c715adcb3a0abda904962210090c
Drikus Roor <drikusroor@gmail.com>
2024-07-23 13:21:02 +0200

    chore: Remove withRouter from FinalButton

--- frontend/src/components/Final/FinalButton.tsx
+++ frontend/src/components/Final/FinalButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface FinalButtonProps {
     button: {
@@ -47,4 +47,4 @@ const FinalButton: React.FC<FinalButtonProps> = ({ button, onNext }) => {
     )
 }
 
-export default withRouter(FinalButton);
+export default FinalButton;
