3527927a6e22e5c99e7559a8cc2b41e154f7b7d7
Drikus Roor <drikusroor@gmail.com>
2024-07-29 14:54:01 +0200

    refactor: Convert _Range component to TypeScript

--- frontend/src/components/Question/_Range.tsx
+++ frontend/src/components/Question/_Range.tsx
@@ -1,10 +1,20 @@
-import React from "react";
 import Slider from "react-rangeslider";
 import classNames from "classnames";
+import Question from "@/types/Question";
 
-// Range is a question view that makes you select a value within the given range, using a slider
-const Range = ({ question, value, onChange}) => {
+interface RangeProps {
+    question: Question;
+    value: number;
+    onChange: (value: number) => void;
+}
+
+/** Range is a question view that makes you select a value within the given range, using a slider */
+const Range = ({ question, value, onChange }: RangeProps) => {
     const emptyValue = !value;
+
+    if (!question.min_value && question.min_value !== 0 || !question.max_value && question.max_value !== 0) {
+        throw new Error('min_value and max_value are required for the Range component');
+    }
 
     if (emptyValue) {
         value = (question.min_value + question.max_value) / 2;
