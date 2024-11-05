5650cf3f0bb712a064c456b153d3eea5a8a7ce8b
Drikus Roor <drikusroor@gmail.com>
2024-11-05 14:23:39 +0100

    style: Improve styling & stories of profile view, rank & cup (#1306)

--- frontend/src/components/Rank/Rank.tsx
+++ frontend/src/components/Rank/Rank.tsx
@@ -1,5 +1,6 @@
 import ScoreCounter, { ScoreCounterProps } from "../ScoreCounter/ScoreCounter";
 import Cup, { CupProps } from "../Cup/Cup";
+import "./Rank.scss";
 
 interface RankProps {
     cup: CupProps
@@ -8,7 +9,7 @@ interface RankProps {
 
 // Rank shows a decorated representation of a rank
 const Rank = ({ cup, score }: RankProps) => (
-    <div data-testid="rank" >
+    <div data-testid="rank" className="aha__rank">
         <Cup className={cup.className} text={cup.text} />
         <ScoreCounter score={score.score} label={score.label} />
     </div >
