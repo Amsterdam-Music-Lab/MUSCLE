22ccf2c260487e461b88c5eaff92119c959808d9
Drikus Roor <drikusroor@gmail.com>
2024-12-05 11:34:15 +0100

    feat: Integrate MSW for API mocking in Storybook and update MatchingPairs story to use handlers

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,4 @@
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import "../public/vendor/bootstrap/bootstrap.min.css";
 import "../src/index.scss";
 import { initAudioListener } from "../src/util/audio";
@@ -6,6 +7,9 @@ import { initWebAudioListener } from "../src/util/webAudio";
 // Init audio listener
 initAudioListener();
 initWebAudioListener();
+
+// Initialize MSW
+initialize()
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -17,6 +21,8 @@ const preview = {
             },
         },
     },
+    // Provide the MSW addon loader globally
+    loaders: [mswLoader],
 };
 
 
