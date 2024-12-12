e22782c9d8b085acb06292310b9d262c4a6a6725
Drikus Roor <drikusroor@gmail.com>
2024-12-12 16:51:55 +0100

    docs: add docstring for Redirect action class

--- backend/experiment/actions/redirect.py
+++ backend/experiment/actions/redirect.py
@@ -2,7 +2,23 @@ from .base_action import BaseAction
 
 
 class Redirect(BaseAction):
-    ID = 'REDIRECT'
+    """
+    Redirect Action
+
+    This action is used to redirect the user to a specified URL.
+
+    Attributes:
+        ID (str): Identifier for the action, set to 'REDIRECT'.
+        url (str): The URL to which the redirection should occur.
+
+    Args:
+        url (str): The URL to redirect to.
+
+    Example:
+        redirect_action = Redirect('https://example.com')
+    """
+
+    ID = "REDIRECT"
 
     def __init__(self, url):
         self.url = url
