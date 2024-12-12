909f584da3be1a67555265ca2de7726768e94336
Drikus Roor <drikusroor@gmail.com>
2024-12-12 16:17:39 +0100

    refactor: Turn off tutorial in matching pairs lite game

--- backend/experiment/rules/matching_pairs_lite.py
+++ backend/experiment/rules/matching_pairs_lite.py
@@ -8,33 +8,29 @@ from experiment.actions.utils import final_action_with_optional_button
 
 
 class MatchingPairsLite(MatchingPairsGame):
-    ID = 'MATCHING_PAIRS_LITE'
+    ID = "MATCHING_PAIRS_LITE"
     num_pairs = 8
     show_animation = False
-    score_feedback_display = 'small-bottom-right'
-    contact_email = 'aml.tunetwins@gmail.com'
+    score_feedback_display = "small-bottom-right"
+    contact_email = "aml.tunetwins@gmail.com"
+    tutorial = None
 
     def next_round(self, session):
         playlist = Playlist(session.block.playlists.all())
-        info = Info('',
-            heading='Press start to enter the game',
-            button_label='Start')
+        info = Info("", heading="Press start to enter the game", button_label="Start")
         if session.get_rounds_passed() < 1:
             trial = self.get_matching_pairs_trial(session)
             return [playlist, info, trial]
         else:
-            return final_action_with_optional_button(session, final_text='End of the game', title='Score', button_text='Back to dashboard')
+            return final_action_with_optional_button(
+                session, final_text="End of the game", title="Score", button_text="Back to dashboard"
+            )
 
     def select_sections(self, session):
-        pairs = list(session.playlist.section_set.order_by().distinct(
-            'group').values_list('group', flat=True))
-        selected_pairs = pairs[:self.num_pairs]
-        originals = session.playlist.section_set.filter(
-            group__in=selected_pairs, tag='Original'
-        )
-        degradations = session.playlist.section_set.exclude(tag='Original').filter(
-            group__in=selected_pairs
-        )
+        pairs = list(session.playlist.section_set.order_by().distinct("group").values_list("group", flat=True))
+        selected_pairs = pairs[: self.num_pairs]
+        originals = session.playlist.section_set.filter(group__in=selected_pairs, tag="Original")
+        degradations = session.playlist.section_set.exclude(tag="Original").filter(group__in=selected_pairs)
         if degradations:
             sections = list(originals) + list(degradations)
             return self.shuffle_sections(sections)
