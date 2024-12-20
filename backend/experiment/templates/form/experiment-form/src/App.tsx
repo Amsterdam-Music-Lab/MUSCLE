80ed900b5fcef96749a6375f680046c3e75b2f39
Drikus Roor <drikusroor@gmail.com>
2024-12-20 13:30:43 +0100

    feat: add react-router-dom for routing and update ExperimentForm integration

--- backend/experiment/templates/form/experiment-form/src/App.tsx
+++ backend/experiment/templates/form/experiment-form/src/App.tsx
@@ -1,37 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ExperimentForm from './components/ExperimentForm'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1 className="text-4xl font-bold text-center">
+        MUSCLE forms
+      </h1>
+      <nav className="flex justify-center space-x-4 mt-8">
+        <a href="/experiments" className="text-blue-500">Create Experiment</a>
+      </nav>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Router>
+          <Routes>
+            {/* Request reload for given participant */}
+            <Route path={'/experiments'} element={<ExperimentForm />} />
+          </Routes>
+        </Router >
       </div>
-      <h1>Vite + React</h1>
-      <div>
-        <ExperimentForm />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
