bfaf8cdcd9e8a71103db26b3ec0413999f099e7c
Drikus Roor <drikusroor@gmail.com>
2025-02-20 18:20:08 +0100

    feat: Enhance navigation layout in App component for improved usability and visual consistency

--- backend/experiment/templates/form/experiment-form/src/App.tsx
+++ backend/experiment/templates/form/experiment-form/src/App.tsx
@@ -47,14 +47,17 @@ function App() {
     <>
       <Router>
         <div className="flex relative">
-          <nav className={`fixed h-screen bg-white shadow-lg transition ${isCollapsed ? 'w-16' : 'w-40 xl:w-48'}`}>
+          <nav className={`flex flex-col fixed h-screen bg-white shadow-lg transition ${isCollapsed ? 'w-16' : 'w-40 xl:w-48'}`}>
             <button
               onClick={() => setIsCollapsed(!isCollapsed)}
               className="w-full p-4 text-left hover:bg-gray-100"
             >
               {isCollapsed ? '→' : '←'}
             </button>
-            <div className={`flex flex-col gap-5 transition ${isCollapsed ? 'p-2' : 'p-4'}`}>
+            <div className={`flex flex-col justify-between gap-5 transition ${isCollapsed ? 'p-2' : 'p-4'}`}>
+              <h1 className={`text-left text-lg font-rajdhani font-bold ${isCollapsed ? 'hidden' : ''}`}>
+                MUSCLE Dashboard
+              </h1>
               <Link to="/experiments"
                 className={`transition text-blue-500 hover:bg-gray-100 rounded flex items-center gap-2`}
                 title="Experiments">
@@ -80,7 +83,9 @@ function App() {
                   {!isCollapsed && 'Logout'}
                 </div>
               </button>
+
             </div>
+
           </nav>
           <div className={`absolute right-0 bg-gray-100 min-h-screen p-4 transition max-w-full ${isCollapsed ? 'left-16' : 'left-40 xl:left-48'}`}>
             <Routes>
