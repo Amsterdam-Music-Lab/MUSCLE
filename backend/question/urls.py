29b60bcdd3ed9a60273ad91e29b71fc093b7c97e
Drikus Roor <drikusroor@gmail.com>
2025-02-20 10:29:02 +0100

    feat: Implement Question and Choice serializers, add QuestionViewSet for API integration

--- backend/question/urls.py
+++ backend/question/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from .views import question_groups
+from django.urls import path, include
+from rest_framework import routers
+from .views import question_groups, QuestionViewSet
 
-app_name = 'question'
+app_name = "question"
+
+router = routers.DefaultRouter()
+router.register(r"question_groups", viewset=question_groups, basename="question_groups")
+
+api_router = routers.DefaultRouter()
+api_router.register(r"questions", QuestionViewSet, basename="question")
 
 urlpatterns = [
-    path('question_groups/', question_groups, name='question_groups'),
+    path("question_groups/", question_groups, name="question_groups"),
+    path("api/", include(api_router.urls)),
 ]
