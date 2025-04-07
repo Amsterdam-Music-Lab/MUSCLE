9d27a1961036997a1fed5cf96c7ca465d80dac3f
BeritJanssen <berit.janssen@gmail.com>
2025-04-07 12:14:47 +0200

    fix expected number of built-in questions

--- backend/question/management/tests.py
+++ backend/question/management/tests.py
@@ -10,9 +10,9 @@ class CreateQuestionsTest(TestCase):
 
     def test_createquestions(self):
         from question.models import Question, QuestionGroup
-        self.assertEqual(len(Question.objects.all()), 161) # Only built-in questions in test database
+        self.assertEqual(
+            len(Question.objects.all()), 176
+        )  # Only built-in questions in test database
         self.assertEqual(len(QuestionGroup.objects.all()), 18) # Only built-in question groups in test database
         self.assertEqual(len(Question.objects.filter(key='dgf_country_of_origin')), 1)
         self.assertEqual(len(QuestionGroup.objects.filter(key='DEMOGRAPHICS')), 1)
-
-
