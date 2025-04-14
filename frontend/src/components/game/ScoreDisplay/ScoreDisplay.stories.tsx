51edf575de062eea5fd727a5cfe320c34b36441d
bacor <accounts@bascornelissen.nl>
2025-04-14 10:07:25 +0200

    ScoreDisplay code formatting and fix zero score

--- frontend/src/components/game/ScoreDisplay/ScoreDisplay.stories.tsx
+++ frontend/src/components/game/ScoreDisplay/ScoreDisplay.stories.tsx
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2025 Bas Cornelissen
  * SPDX-License-Identifier: MIT
- * 
+ *
  * This file is part of the MUSCLE project by Amsterdam Music Lab.
  * Licensed under the MIT License. See LICENSE file in the project root.
  */
@@ -10,7 +10,7 @@ import ScoreDisplay from "./ScoreDisplay";
 
 const decorator = (Story) => (
   <div style={{ padding: "1rem" }}>
-      <Story />
+    <Story />
   </div>
 );
 
@@ -18,75 +18,81 @@ export default {
   title: "Game UI/ScoreDisplay",
   component: ScoreDisplay,
   parameters: {
-      layout: "fullscreen",
+    layout: "fullscreen",
   },
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 };
 
 export const Default = {
   args: {
     score: 321,
-    label: "Your score"
+    label: "Your score",
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
-
 
 export const ExtraLarge = {
   args: {
     score: 321,
     label: "Your score",
     units: "points",
-    size: 6
+    size: 6,
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
 export const NoScore = {
   args: {
     label: "Your score",
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
+export const ZeroScore = {
+  args: {
+    label: "Your score",
+    score: 0,
+  },
+  decorators: [decorator],
+};
 
 export const NegativeScore = {
   args: {
     score: -200,
     label: "Another score",
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
 export const HTMLAttributes = {
   args: {
     score: -200,
     label: "Another score",
-    style: { background: "#fff", padding: "1em"  },
+    style: { background: "#fff", padding: "1em" },
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
 export const NoVariant = {
   args: {
     score: -200,
-    variant: null
+    variant: null,
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
 export const PrimaryVariant = {
   args: {
     score: -200,
-    variant: "primary"
+    variant: "primary",
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
 
 export const SecondaryVariant = {
   args: {
     score: -200,
-    variant: "secondary"
+    variant: "secondary",
   },
-  decorators: [decorator]
+  decorators: [decorator],
 };
--- frontend/src/components/game/ScoreDisplay/ScoreDisplay.tsx
+++ frontend/src/components/game/ScoreDisplay/ScoreDisplay.tsx
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2025 Bas Cornelissen
  * SPDX-License-Identifier: MIT
- * 
+ *
  * This file is part of the MUSCLE project by Amsterdam Music Lab.
  * Licensed under the MIT License. See LICENSE file in the project root.
  */
 
-import React from "react"
+import React from "react";
 import classNames from "classnames";
 import { Variant } from "@/theme/themes";
-import "./ScoreDisplay.scss"
+import "./ScoreDisplay.scss";
 
 interface ScoreProps extends React.HTMLAttributes<HTMLDivElement> {
   /** The score */
@@ -25,37 +25,45 @@ interface ScoreProps extends React.HTMLAttributes<HTMLDivElement> {
   placeholder?: string;
 
   /** The size of the score in rems. Default 4 */
-  size: number;
+  size?: number;
 
   /** The variant color */
-  variant: Variant;
+  variant?: Variant;
 }
 
 /**
  * A large score with optional units as superscript and an optional label below.
  * When no score is passed, a placeholder text (default "??") is shown.
  */
-export default function ScoreDisplay({ 
-  score, 
-  label, 
+export default function ScoreDisplay({
+  score,
+  label,
   units = "pts",
-  size = 4, 
-  placeholder="??",
-  variant="primary",
+  size = 4,
+  placeholder = "??",
+  variant = "primary",
   ...props
 }: ScoreProps) {
   const { className, style, ...rest } = props;
   return (
-    <div 
-      className={classNames("score-display", className)} 
-      style={{"--score-display-font-size": size, ...style }} 
+    <div
+      className={classNames("score-display", className)}
+      style={{ "--score-display-font-size": size, ...style }}
       {...rest}
     >
       <div className="score">
-        <div className={`value ${score ? `text-fill-${variant}` : 'text-light-gray'}`}>{score || placeholder}</div>
-        {(units && score) && <span className={`text-fill-${variant} units small`}>{units}</span>}
+        <div
+          className={`value ${
+            score !== undefined ? `text-fill-${variant}` : "text-light-gray"
+          }`}
+        >
+          {score === undefined ? placeholder : score}
+        </div>
+        {units && score !== undefined ? (
+          <span className={`text-fill-${variant} units small`}>{units}</span>
+        ) : null}
       </div>
       {label && <div className="label text-muted small">{label}</div>}
     </div>
-  )
+  );
 }
