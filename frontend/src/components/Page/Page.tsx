3eb4a0f9ca49d3527f4f38ad3d8b9d89159c4753
bacor <mail@bascornelissen.nl>
2025-04-16 18:35:09 +0200

    Fix gradient circles and use blurred, primary variant

--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -4,30 +4,38 @@ import classNames from "classnames";
 import { GradientCircles } from "@/components/svg";
 
 interface PageProps {
-    className?: string;
-    children: React.ReactNode;
+  className?: string;
+  children: React.ReactNode;
 }
 
 /** Page is a single page in the application */
 const Page = ({ className, children }: PageProps) => {
+  const theme = useBoundStore((state) => state.theme);
 
-    const theme = useBoundStore((state) => state.theme);
-    
-    // @BC mark MCG theme as .mcg
-    // @BC no default bg '/public/images/background.jpg', instead add .no-background
-    const hasBackground = Boolean(theme?.backgroundUrl)
+  // @BC mark MCG theme as .mcg
+  // @BC no default bg '/public/images/background.jpg', instead add .no-background
+  const hasBackground = Boolean(theme?.backgroundUrl);
 
-    return (
-        <>
-            <div 
-                className={classNames('aha__page', className, !hasBackground && 'no-background', theme?.name == "MCG" && "mcg" )} 
-                style={!hasBackground ? null : {backgroundImage: `url(${theme.backgroundUrl})`} }
-            >
-                {children}
-            </div>
-            {theme?.name == "MCG" && <GradientCircles color1="yellow" color2="pink" numCircles={30} />}
-        </>
-    );
+  return (
+    <>
+      <div
+        className={classNames(
+          "aha__page",
+          className,
+          !hasBackground && "no-background",
+          theme?.name == "MCG" && "mcg"
+        )}
+        style={
+          !hasBackground
+            ? null
+            : { backgroundImage: `url(${theme.backgroundUrl})` }
+        }
+      >
+        {children}
+      </div>
+      {theme?.name == "MCG" && <GradientCircles blur={50} animate={true} />}
+    </>
+  );
 };
 
 export default Page;
