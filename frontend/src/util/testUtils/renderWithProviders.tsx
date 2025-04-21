d13048b8e69e25468a724c9964bab5fa43049d71
bacor <accounts@bascornelissen.nl>
2025-04-21 11:34:19 +0200

    Update rendering to allow custom wrappers

--- frontend/src/util/testUtils/renderWithProviders.tsx
+++ frontend/src/util/testUtils/renderWithProviders.tsx
@@ -1,17 +1,30 @@
-import { ReactNode } from 'react'
-import { render, RenderOptions } from '@testing-library/react'
-import { ThemeProvider } from '@/theme/ThemeProvider'
+/**
+ * Copyright (c) 2025 Amsterdam Music Lab
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of the MUSCLE project by Amsterdam Music Lab.
+ * Licensed under the MIT License. See LICENSE file in the project root.
+ */
+
+import type { ReactNode } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { ThemeProvider } from "@/theme/ThemeProvider";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   // Add other providers if needed
-  return <ThemeProvider>{children}</ThemeProvider>
-}
+  return <ThemeProvider>{children}</ThemeProvider>;
+};
 
 export const renderWithProviders = (
   ui: React.ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>
-) =>
-  render(ui, {
-    wrapper: Providers,
-    ...options,
-  })
+  options: RenderOptions = {}
+) => {
+  let { wrapper, ...rest } = options;
+  if (!wrapper) {
+    wrapper = ({ children }) => children;
+  }
+  return render(ui, {
+    wrapper: ({ children }) => <Providers children={wrapper({ children })} />,
+    ...rest,
+  });
+};
