5aadb2ec72eceef7d86204288d5e468f9d2d2daa
bacor <accounts@bascornelissen.nl>
2025-05-19 11:45:02 +0200

    Code formatting

--- frontend/src/components/application/Page/Page.tsx
+++ frontend/src/components/application/Page/Page.tsx
@@ -1,4 +1,12 @@
-import type { HTMLAttributes, ReactNode } from "react";
+/**
+ * Copyright (c) 2025 Amsterdam Music Lab
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of the MUSCLE project by Amsterdam Music Lab.
+ * Licensed under the MIT License. See LICENSE file in the project root.
+ */
+
+import type { HTMLAttributes } from "react";
 import classNames from "classnames";
 import useBoundStore from "@/util/stores";
 import { useTheme } from "@/theme/ThemeProvider";
