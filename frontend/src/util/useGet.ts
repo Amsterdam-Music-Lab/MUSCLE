90505e75c08264ce433d9f7f6f937e86f4f8e2d9
bacor <accounts@bascornelissen.nl>
2025-06-05 20:55:20 +0200

    formatting

--- frontend/src/util/useGet.ts
+++ frontend/src/util/useGet.ts
@@ -2,35 +2,35 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 // useGet is a react hook for getting data from a given url
-export const useGet = <T,>(url: string): [T | null, boolean] => {
-    const [data, setData] = useState<T | null>(null);
-    const [loading, setLoading] = useState(true);
+export const useGet = <T>(url: string): [T | null, boolean] => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
+    setData(null);
+    setLoading(true);
+
+    const source = axios.CancelToken.source();
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+        setLoading(false);
+      } catch (err) {
         setData(null);
-        setLoading(true);
+        setLoading(false);
+      }
+    };
+    fetchData();
 
-        const source = axios.CancelToken.source();
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(url);
-                setData(response.data);
-                setLoading(false);
-            } catch (err) {
-                setData(null);
-                setLoading(false);
-            }
-        };
-        fetchData();
+    return () => {
+      setData(null);
+      setLoading(false);
+      source.cancel();
+    };
+  }, [url]);
 
-        return () => {
-            setData(null);
-            setLoading(false);
-            source.cancel();
-        };
-    }, [url]);
-
-    return [data, loading];
+  return [data, loading];
 };
 
 export default useGet;
