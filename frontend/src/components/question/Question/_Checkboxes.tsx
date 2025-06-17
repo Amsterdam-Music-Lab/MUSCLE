a2de1a39bf2045e5daaf7e981f61aae84b2d6e22
bacor <accounts@bascornelissen.nl>
2025-06-17 23:31:05 +0200

    fix: consistent formatting of checkbox and radio inputs

--- frontend/src/components/question/Question/_Checkboxes.tsx
+++ frontend/src/components/question/Question/_Checkboxes.tsx
@@ -1,79 +1,78 @@
 import type Question from "@/types/Question";
 import classNames from "classnames";
+import styles from "./_Checkboxes.module.scss";
 
 interface CheckboxesProps {
-    question: Question;
-    value: string;
-    onChange: (value: string) => void;
+  question: Question;
+  value: string;
+  onChange: (value: string) => void;
 }
 
 /** Checkboxes is a question view for selecting multiple options from a list */
 const Checkboxes = ({ question, value, onChange }: CheckboxesProps) => {
+  const choices = question.choices;
 
-    const choices = question.choices;
+  if (!choices || Object.keys(choices).length <= 0) {
+    throw new Error("Checkboxes question must have choices");
+  }
 
-    if (!choices || Object.keys(choices).length <= 0) {
-        throw new Error("Checkboxes question must have choices");
+  const values = value ? value.split(",") : [];
+
+  // Add/remove value
+  const onToggle = (value: string) => {
+    const index = values.indexOf(value);
+    if (index === -1) {
+      values.push(value);
+    } else {
+      values.splice(index, 1);
     }
+    onChange(values.join(","));
+  };
 
-    const values = value ? value.split(",") : [];
-
-    // Add/remove value
-    const onToggle = (value: string) => {
-        const index = values.indexOf(value);
-        if (index === -1) {
-            values.push(value);
-        } else {
-            values.splice(index, 1);
-        }
-        onChange(values.join(","));
-    };
-
-    return (
-        <div className="aha__checkboxes">
-            {Object.keys(choices).map((val, index) => (
-                <Checkbox
-                    key={index}
-                    // This prop does not exist on Checkbox
-                    name={question.key}
-                    label={choices[val]}
-                    value={val}
-                    checked={values.includes(val)}
-                    onChange={onToggle}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.checkboxes}>
+      {Object.keys(choices).map((val, index) => (
+        <Checkbox
+          key={index}
+          // This prop does not exist on Checkbox
+          name={question.key}
+          label={choices[val]}
+          value={val}
+          checked={values.includes(val)}
+          onChange={onToggle}
+        />
+      ))}
+    </div>
+  );
 };
 
 interface CheckboxProps {
-    label: string;
-    value: string;
-    checked: boolean;
-    onChange: (value: string) => void;
+  label: string;
+  value: string;
+  checked: boolean;
+  onChange: (value: string) => void;
 }
 
 /** Checkbox is a single checkbox */
 const Checkbox = ({ label, value, checked, onChange }: CheckboxProps) => {
-
-    const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        // Enter or space
-        if (event.key === "Enter" || event.key === " ") {
-            onChange(value);
-        }
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    // Enter or space
+    if (event.key === "Enter" || event.key === " ") {
+      onChange(value);
     }
+  };
 
-    return (
-        <div
-            className={classNames("checkbox", { checked })}
-            onClick={() => onChange(value)}
-            tabIndex={0}
-            onKeyDown={handleKeyPress}
-        >
-            <i></i>
-            <span>{label}</span>
-        </div>
-    );
+  return (
+    <div
+      className={classNames(styles.checkbox, checked && styles.checked)}
+      onClick={() => onChange(value)}
+      tabIndex={0}
+      onKeyDown={handleKeyPress}
+    >
+      <i></i>
+      <span>{label}</span>
+    </div>
+  );
 };
 
 export default Checkboxes;
