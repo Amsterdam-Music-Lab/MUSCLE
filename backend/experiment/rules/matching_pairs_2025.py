7320e81789f1519483e913a0b73731fcd5bd6543
BeritJanssen <berit.janssen@gmail.com>
2025-06-20 17:11:53 +0200

    add questionnaire rules

--- backend/experiment/rules/matching_pairs_2025.py
+++ backend/experiment/rules/matching_pairs_2025.py
@@ -2,7 +2,6 @@ from os.path import split
 import random
 
 from django.utils.translation import gettext_lazy as _
-from django.db import models
 
 from result.models import Result
 from section.models import Section
