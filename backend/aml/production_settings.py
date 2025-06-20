be7e8ff6528febcdb50956eb3b4ed20f322285f3
BeritJanssen <berit.janssen@gmail.com>
2025-06-20 17:53:40 +0200

    change RELOAD_PARTICIPANT_TARGET

--- backend/aml/production_settings.py
+++ backend/aml/production_settings.py
@@ -64,7 +64,7 @@ LOGGING = {
     }
 }
 
-RELOAD_PARTICIPANT_TARGET = 'http://app.amsterdammusiclab.nl'
+RELOAD_PARTICIPANT_TARGET = 'http://tunetwins.amsterdammusiclab.nl'
 
 TESTING = os.getenv('AML_TESTING', '') != 'False'
 
