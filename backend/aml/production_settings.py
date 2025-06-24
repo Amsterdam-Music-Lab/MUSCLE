ae0c7eb8c5914164b3bd53992d7877ae3369f1c0
BeritJanssen <berit.janssen@gmail.com>
2025-06-24 11:55:17 +0200

    fix: change `RELOAD_PARTICIPANT_TARGET` to relative path for redirect

--- backend/aml/production_settings.py
+++ backend/aml/production_settings.py
@@ -64,7 +64,7 @@ LOGGING = {
     }
 }
 
-RELOAD_PARTICIPANT_TARGET = 'https://tunetwins.amsterdammusiclab.nl/tunetwins/'
+RELOAD_PARTICIPANT_TARGET = '/tunetwins/'
 
 TESTING = os.getenv('AML_TESTING', '') != 'False'
 
