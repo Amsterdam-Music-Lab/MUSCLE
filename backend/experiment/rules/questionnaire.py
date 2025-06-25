23c462e6f717bcc0c844f42aabd1c2b462cd1e9d
bacor <accounts@bascornelissen.nl>
2025-06-25 11:01:28 +0200

    temporary hacky fix: redirect to a random other block at end of questionnaire

--- backend/experiment/rules/questionnaire.py
+++ backend/experiment/rules/questionnaire.py
@@ -4,11 +4,14 @@ from django.utils.translation import gettext_lazy as _
 from .base import BaseRules
 from session.models import Session
 from experiment.actions import Explainer, Redirect
+from random import choice
+
 
 class Questionnaire(BaseRules):
-    """ a rules file which does not do anything but show a questionnaire """
-    ID = 'QUESTIONNAIRE'
-    debrief_form = 'final/debrief_rhythm_unpaid.html'
+    """a rules file which does not do anything but show a questionnaire"""
+
+    ID = "QUESTIONNAIRE"
+    debrief_form = "final/debrief_rhythm_unpaid.html"
     show_participant_final = False
 
     def next_round(self, session: Session):
@@ -25,5 +28,18 @@ class Questionnaire(BaseRules):
             )
             actions.append(intro_questions)
             actions.extend(questions)
-        actions.append(Redirect(settings.RELOAD_PARTICIPANT_TARGET))
+
+        # Temporary hack: just redirect to a random block.
+        blocks = session.block.phase.experiment.associated_blocks()
+        other_blocks = []
+        for block in blocks:
+            rules = block.get_rules()
+            if rules.__class__ != self.__class__:
+                other_blocks += [block]
+        next_block = choice(other_blocks)
+        experiment_url = self.get_experiment_url(session)
+        actions.append(
+            Redirect(f"{experiment_url}/block/{next_block.slug}")
+            # Redirect(settings.RELOAD_PARTICIPANT_TARGET)
+        )
         return actions
