da91e33da681b4e45fafedda33143aef0659c2ac
bacor <mail@bascornelissen.nl>
2025-07-18 09:43:30 +0200

    Read lingui config from i18n.ts

--- frontend/lingui.config.js
+++ frontend/lingui.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "@lingui/cli";
+import { locales, defaultLocale } from "./src/i18n";
 
 export default defineConfig({
-  sourceLocale: "en",
-  locales: ["en", "nl"],
+  sourceLocale: defaultLocale,
+  locales: Object.keys(locales),
   catalogs: [
     {
       path: "<rootDir>/src/locales/{locale}/messages",
