d99d72630821bf39f5c36e4b7a44111d0f04bcc1
BeritJanssen <berit.janssen@gmail.com>
2025-12-03 10:45:32 +0100

    fix: frontend NUMBER question type added

--- frontend/src/components/Question/_String.tsx
+++ frontend/src/components/Question/_String.tsx
@@ -1,5 +1,5 @@
 import Question from "@/types/Question";
-import { useRef, useEffect } from "react";
+import Input from "./_Input";
 
 interface StringProps {
     question: Question;
@@ -9,63 +9,21 @@ interface StringProps {
 
 /** String is a question view that lets you input text */
 const String = ({ question, value = "", onChange }: StringProps) => {
-    const input = useRef<HTMLInputElement>(null);
-
-    if (question.input_type === 'number' && ((!question.min_value && question.min_value !== 0) || (!question.max_value && question.max_value !== 0))) {
-        throw new Error('min_value and max_value are required for the String component with input type is "number"');
-    }
-
-    if (question.input_type === 'text' && !question.max_length && question.max_length !== 0) {
+    
+    if (!question.max_length && question.max_length !== 0) {
         throw new Error('max_length is required for the String component with input type is "text"');
     }
 
     // Input validation
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const validateChange = (currentValue: string) => {
 
-        if (!input.current) {
-            return;
-        }
-
-        switch (question.input_type) {
-            case 'number':
-                if (input.current.value >= question.min_value && input.current.value <= question.max_value) {
-                    onChange(input.current.value);
-                }
-                break;
-            case 'text':
-                if (question.max_length && input.current.value.length > question.max_length) {
-                    break;
-                }
-                onChange(input.current.value);
-                break;
-            default:
-                onChange(input.current.value);
+        if (currentValue.length < question.max_length) {
+            onChange(currentValue);
         }
     };
 
-    /** Key validation */
-    const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-        }
-    }
-
-    useEffect(() => {
-        if (input.current) {
-            input.current.focus();
-        }
-    }, []);
-
     return (
-        <div className="aha__string">
-            <input
-                type={question.input_type}
-                value={value}
-                ref={input}
-                onChange={handleChange}
-                onKeyDown={(e) => handleKey(e)}
-            />
-        </div>
+        <Input validateChange={validateChange} type="text" value={value}/>
     );
 };
 
