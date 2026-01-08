77861dc4a1382bfd8db9c6d9ed44c79fcb262b8b
BeritJanssen <berit.janssen@gmail.com>
2026-01-08 17:25:44 +0100

    chore: use uv in lieu of pip (#1594)

--- backend/manage.py
+++ backend/manage.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import sys
-from os.path import join, dirname
-from dotenv import load_dotenv
 
 
 def main():
-    env_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path=env_path)
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
