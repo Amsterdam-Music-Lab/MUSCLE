d48192081a23b7c49ffe7bcd9bd198de10941b85
BeritJanssen <berit.janssen@gmail.com>
2026-02-02 12:57:59 +0100

    remove dotenv from asgi config

--- backend/aml/asgi.py
+++ backend/aml/asgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-from dotenv import load_dotenv
 from pathlib import Path
 import os
 
@@ -15,6 +14,5 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aml.production_settings')
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 
 application = get_asgi_application()
