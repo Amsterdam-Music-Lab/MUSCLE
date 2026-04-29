a3fd348c56b75364794c227bd2845aba487e8f54
BeritJanssen <berit.janssen@gmail.com>
2026-04-29 15:36:41 +0200

    remove unique constraint on `block.slug`

--- backend/experiment/forms.py
+++ backend/experiment/forms.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.forms import (
     CheckboxSelectMultiple,
     ChoiceField,
@@ -48,18 +48,22 @@ class BlockForm(ModelForm):
 
         return playlists
 
+    def clean_slug(self):
+        print("phase", self.instance.phase)
+        return self.cleaned_data.get("slug")
+
     class Meta:
         model = Block
         fields = [
             "index",
             "slug",
+            "rules",
             "name",
             "description",
             "image",
             "rounds",
             "bonus_points",
             "playlists",
-            "rules",
             "rules_config",
             "theme_config",
         ]
@@ -69,8 +73,8 @@ class BlockForm(ModelForm):
                 "An image that will be displayed on the experiment page and as a meta image in search engines."
             ),
             "slug": _(
-                "The slug is used to identify the block in the URL so you can access it on the web as follows: app.amsterdammusiclab.nl/{slug} <br>\
-            It must be unique, lowercase and contain only letters, numbers, and hyphens. Nor can it start with any of the following reserved words: admin, server, block, participant, result, section, session, static."
+                "The slug is used to identify the block in the URL so you can access it on the web as follows: app.amsterdammusiclab.nl/{experiment-slug}/block/{slug} <br>\
+            It must be unique within its experiment, lowercase and contain only letters, numbers, and hyphens. Nor can it start with any of the following reserved words: admin, server, block, participant, result, section, session, static."
             ),
             "rules_config": _(
                 "This field can be used to enter dictionaries to configure behaviour of rules. Refer to the rules files' source code for possible settings."
--- backend/experiment/tests/test_forms.py
+++ backend/experiment/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from experiment.forms import BlockForm, BLOCK_RULES
-
+from experiment.models import Block, Experiment, Phase
 
 class BlockFormTest(TestCase):
     def test_form_fields(self):
@@ -17,3 +17,23 @@ class BlockFormTest(TestCase):
         expected_choices = [(i, BLOCK_RULES[i].__name__) for i in BLOCK_RULES]
         expected_choices.append(("", "---------"))
         self.assertEqual(form.fields['rules'].choices, sorted(expected_choices))
+
+    def test_clean_slug(self):
+        experiment = Experiment.objects.create(slug="test")
+        phase = Phase.objects.create(experiment=experiment)
+
+        Block.objects.create(phase=phase, slug="test1")
+        block2 = Block.objects.create(phase=phase)
+
+        form_data = {
+            "slug": "test1",
+            "name": "Test Block",
+            "index": 1,
+            "rules": "QUESTIONNAIRE",
+            "rounds": 1,
+            "bonus_points": 0,
+        }
+        form = BlockForm(form_data, instance=block2)
+        self.assertFalse(form.is_valid())
+        self.assertIn("slug", form.errors.keys())
+        self.assertIn("not unique", form.errors.get("slug")[0])
