2fa0aa3942083a6fb7b4c52297fdf37481e04ed9
BeritJanssen <berit.janssen@gmail.com>
2026-05-20 16:34:50 +0200

    add admin interface

--- backend/action/models.py
+++ backend/action/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Explainer(models.Model):
+    identifier = models.SlugField(max_length=64, unique=True)
     instruction = models.CharField(max_length=256, blank=True, default="")
     timer = models.IntegerField(blank=True, default=0)
     number_steps = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return self.identifier
+
 
 class Step(models.Model):
     explainer = models.ForeignKey(Explainer, on_delete=models.CASCADE, related_name="steps")
