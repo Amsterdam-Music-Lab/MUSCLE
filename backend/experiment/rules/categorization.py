466952a2a45cd912ad4eaa2742fdd0144e1f2786
BeritJanssen <berit.janssen@gmail.com>
2026-08-25 14:42:16 +0200

    fix: categorization timeout feedback, button width

--- backend/experiment/rules/categorization.py
+++ backend/experiment/rules/categorization.py
@@ -263,8 +263,16 @@ class Categorization(BaseRules):
         colors = ['colorNeutral1', 'colorNeutral2']
         random.shuffle(colors)
         choices = [
-            {"value": "A", "label": "", "color": colors[0]},
-            {"value": "B", "label": "", "color": colors[1]},
+            {
+                "value": "A",
+                "label": "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0",
+                "color": colors[0],
+            },
+            {
+                "value": "B",
+                "label": "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0",
+                "color": colors[1],
+            },
         ]
         random.shuffle(choices)
         if group == "S1":
@@ -392,7 +400,7 @@ class Categorization(BaseRules):
         last_score = session.last_score()
 
         if session.last_result().given_response == "TIMEOUT":
-            icon = "?"
+            icon = render_to_string("images/categorization/question-solid.svg")
         elif last_score == 1:
             icon = render_to_string("images/categorization/face-smile-solid.svg")
         elif last_score == 0:
