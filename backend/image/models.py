f0340082b249a9358c96a6bdbc20b438f7a16332
BeritJanssen <berit.janssen@gmail.com>
2026-08-27 15:41:54 +0200

    chore: remove spurious import

--- backend/image/models.py
+++ backend/image/models.py
@@ -1,5 +1,3 @@
-from os.path import join
-
 from django.contrib.postgres.fields import ArrayField
 from django.core.files.storage import default_storage
 from django.core.files.uploadedfile import UploadedFile
